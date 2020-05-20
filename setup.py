db38ec5800250e5e708325a2f0ad68c9b9d46564
samhainsworth <SamuelHainsworth@users.noreply.github.com>
2020-05-20 16:28:26 +1000

    associated setup updates

--- setup.py
+++ setup.py
@@ -1,26 +1,20 @@
 import data
 import parameters
+import popdict
 
 
 def setup(root, file_name, setting, epidata_loc='epi_data'):
 
     databook = data.load_databook(root, file_name)
 
-    # read in and store parameters
-    pars, metapars, extrapars, layerchars = data.read_params(databook)
-    imported_cases, daily_tests = data.read_tests_imported(databook)
-    params = parameters.Parameters(setting=setting,
-                                   pars=pars,
-                                   metapars=metapars,
-                                   extrapars=extrapars,
-                                   layerchars=layerchars,
-                                   imported_cases=imported_cases,
-                                   daily_tests=daily_tests,
-                                   epidata_loc=epidata_loc)
+    params = parameters.setup_params(databook, setting, epidata_loc)
 
     # read in a store population-related data
     age_dist, household_dist = data.read_popdata(databook)
-    # transform data
 
-    # popdata = population_data.PopulationData()
-    return params # , popdata
+    # get mixing matrix
+    mixing_matrix, bin_lower, bin_upper = data.read_mixing_matrix(databook)
+
+    # popdict.get_popdict()
+
+    return params
