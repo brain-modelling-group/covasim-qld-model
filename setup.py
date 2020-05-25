ee0dd773acc87ba95cd5ef77799897d095a61ebd
samhainsworth <SamuelHainsworth@users.noreply.github.com>
2020-05-25 11:43:34 +1000

    Removed popdict script in favour of a setup function

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
+import contacts as co
 import parameters
-import popdict
+
+
+def get_popdict(params):
+
+    contacts, ages, uids = co.make_contacts(params)
+
+    # create the popdict object
+    popdict = {}
+    popdict['contacts'] = contacts
+    popdict['age'] = ages  # TODO: check this
+    popdict['uid'] = uids
+
+    return popdict
 
 
 def setup(root, file_name, setting, epidata_loc='epi_data'):
 
     params = parameters.setup_params(root, file_name, setting, epidata_loc)
 
-    popdict.get_popdict(params)
+    popdict = get_popdict(params)
 
     return
