d35ac59caf8c982c08819718b948a0a408284e9e
samhainsworth <SamuelHainsworth@users.noreply.github.com>
2020-05-25 11:31:36 +1000

    Moved to appropriate file

--- contacts.py
+++ contacts.py
@@ -134,19 +134,20 @@ def random_contacts(include, mean_contacts_per_person, array_output:bool=False):
 
 def make_custom_contacts(uids, n_contacts, pop_size, ages, custom_lkeys, cluster_types, pop_proportion, age_lb, age_ub):
     contacts = {}
-
     for layer_key in custom_lkeys:
         cl_type = cluster_types[layer_key]
         num_contacts = n_contacts[layer_key]
         custom_clusters, in_layer = cl.make_custom_clusters(uids, pop_size, ages, custom_lkeys, pop_proportion, age_lb, age_ub)
         # handle the cluster types differently
-        if cl_type == 'complete':   # number of contacts not used for complete
+        print(layer_key)
+        if cl_type == 'complete':   # number of contacts not used for complete clusters
+            # TODO: believe big slow-down when making complete pSport network, since 44 contacts per person.
             contacts[layer_key] = clusters_to_contacts([custom_clusters])
         elif cl_type == 'random':
             contacts[layer_key] = random_contacts(in_layer, num_contacts)
         elif cl_type == 'cluster':
-            in_layer_inds = np.where(in_layer)
-            clus = [cl.create_clustering(in_layer_inds, num_contacts)]
+            in_layer_inds = np.where(in_layer)[0] #todo: currently return tuple (error), also could be more efficient doing this earlier
+            clus = cl.create_clustering(in_layer_inds, num_contacts)
             contacts[layer_key] = clusters_to_contacts(clus)
         else:
             raise Exception(f'Error: Unknown network structure: {cl_type}')
@@ -154,3 +155,107 @@ def make_custom_contacts(uids, n_contacts, pop_size, ages, custom_lkeys, cluster
     return contacts
 
 
+def convert_contacts(contacts, uids, all_lkeys):
+    # convert to structure compatible with Covasim
+    # iterate over all the layers, check if UID is a key, if not, add an empty array
+    # basically, want to remove UID being a key to being the index in outer_list
+    contacts_list = [None] * len(uids)
+    for layer_key in all_lkeys:
+        layer_contacts = contacts[layer_key]
+        for uid in uids:
+            if layer_contacts.get(uid) is not None:
+                contacts_list[uid] = layer_contacts[uid]
+            else:
+                contacts_list[uid] = np.empty(0)
+    return contacts_list
+
+
+def get_uids(pop_size):
+    people_id = np.arange(start=0, stop=pop_size, step=1)
+    return people_id
+
+
+def get_numhouseholds(household_dist, pop_size):
+    """Calculates the number of households we need to create to meet the population size"""
+    n_people = sum(household_dist.index * household_dist)  # n_people = household_size * n_households
+    household_percent = household_dist / n_people
+    n_households = (pop_size * household_percent).round().astype(int)
+    n_households[1] += pop_size - sum(n_households * n_households.index)  # adjust single-person households to fill the gap
+    return n_households
+
+
+def get_household_heads(age_dist, n_households):
+    """Selects the ages of the household heads by randomly selecting from the available ages"""
+    # prevent anyone under the age of 18 being chosen
+    age_dist.iloc[0:18] = 0
+    # decrease probability of someone aged 18-28 being chosen
+    age_dist.iloc[18:28] *= np.linspace(0.1, 1, 10)
+
+    # randomly choose household head, given the number of people in each age
+    age_prob = age_dist.values / sum(age_dist.values)
+    household_heads = np.random.choice(age_dist.index, size=sum(n_households), p=age_prob)
+    return household_heads
+
+
+def make_contacts(params):
+    contacts = {}
+
+    pop_size = params.pars['pop_size']
+    household_dist = params.household_dist
+    age_dist = params.age_dist
+    contact_matrix = params.contact_matrix
+    n_contacts = params.pars['contacts']
+    all_lkeys = params.all_lkeys
+
+    # for custom layers
+    custom_lkeys = params.custom_lkeys
+    cluster_types = params.layerchars['cluster_type']
+    pop_proportion = params.layerchars['proportion']
+    age_lb = params.layerchars['age_lb'] # todo: potentially confusing with the age_up in the contact matrix
+    age_ub = params.layerchars['age_ub']
+
+    uids = get_uids(pop_size)
+
+    # household contacts
+    n_households = get_numhouseholds(household_dist, pop_size)
+    household_heads = get_household_heads(age_dist, n_households)
+    h_contacts, ages = make_hcontacts(n_households,
+                                      pop_size,
+                                      household_heads,
+                                      uids,
+                                      contact_matrix)
+    contacts['H'] = h_contacts
+
+    # school contacts
+    key = 'S'
+    social_no = n_contacts[key]
+    s_contacts = make_scontacts(uids, ages, social_no)
+    contacts[key] = s_contacts
+
+    # workplace contacts
+    key = 'W'
+    work_no = n_contacts[key]
+    w_contacts = make_wcontacts(uids, ages, work_no)
+    contacts[key] = w_contacts
+
+    # random community contacts
+    key = 'C'
+    com_no = n_contacts[key]
+    c_contacts = random_contacts(ages, com_no)
+    contacts[key] = c_contacts
+
+    # Custom layers: those that are not households, work, school or community
+    custom_contacts = make_custom_contacts(uids,
+                                           n_contacts,
+                                           pop_size,
+                                           ages,
+                                           custom_lkeys,
+                                           cluster_types,
+                                           pop_proportion,
+                                           age_lb,
+                                           age_ub)
+    contacts.update(custom_contacts)
+
+    contacts_list = convert_contacts(contacts, uids, all_lkeys)
+
+    return contacts_list, ages, uids
--- popdict.py
+++ popdict.py
@@ -1,81 +1,14 @@
-import numpy as np
 import contacts as co
 
 
-def get_numhouseholds(household_dist, pop_size):
-    """Calculates the number of households we need to create to meet the population size"""
-    n_people = sum(household_dist.index * household_dist)  # n_people = household_size * n_households
-    household_percent = household_dist / n_people
-    n_households = (pop_size * household_percent).round().astype(int)
-    n_households[1] += pop_size - sum(n_households * n_households.index)  # adjust single-person households to fill the gap
-    return n_households
-
-
-def get_household_heads(age_dist, n_households):
-    """Selects the ages of the household heads by randomly selecting from the available ages"""
-    # prevent anyone under the age of 18 being chosen
-    age_dist.iloc[0:18] = 0
-    # decrease probability of someone aged 18-28 being chosen
-    age_dist.iloc[18:28] *= np.linspace(0.1, 1, 10)
-
-    # randomly choose household head, given the number of people in each age
-    age_prob = age_dist.values / sum(age_dist.values)
-    household_heads = np.random.choice(age_dist.index, size=sum(n_households), p=age_prob)
-    return household_heads
-
-
-def get_uids(pop_size):
-    people_id = np.arange(start=0, stop=pop_size, step=1)
-    return people_id
-
-
 def get_popdict(params):
-    contacts = {}
+    popdict = {}
 
-    pop_size = params.pars['pop_size']
-    household_dist = params.household_dist
-    age_dist = params.age_dist
-    contact_matrix = params.contact_matrix
-    n_contacts = params.pars['contacts']
+    contacts, ages, uids = co.make_contacts(params)
 
-    uids = get_uids(pop_size)
+    # create the popdict object
+    popdict['contacts'] = contacts
+    popdict['age'] = ages # TODO: check this
+    popdict['uid'] = uids
 
-    # household contacts
-    n_households = get_numhouseholds(household_dist, pop_size)
-    household_heads = get_household_heads(age_dist, n_households)
-    h_contacts, ages = co.make_hcontacts(n_households,
-                                         pop_size,
-                                         household_heads,
-                                         uids,
-                                         contact_matrix)
-    contacts['H'] = h_contacts
-
-    # school contacts
-    key = 'S'
-    social_no = n_contacts[key]
-    s_contacts = co.make_scontacts(uids, ages, social_no)
-    contacts[key] = s_contacts
-
-    # workplace contacts
-    key = 'W'
-    work_no = n_contacts[key]
-    w_contacts = co.make_wcontacts(uids, ages, work_no)
-    contacts[key] = w_contacts
-
-    # random community contacts
-    key = 'C'
-    com_no = n_contacts[key]
-    c_contacts = co.random_contacts(ages, com_no)
-    contacts[key] = c_contacts
-
-    # Custom layers: those that are not households, work, school or community
-    custom_lkeys = params.custom_lkeys
-    cluster_types = params.layerchars['cluster_type']
-    pop_proportion = params.layerchars['proportion']
-    age_lb = params.layerchars['age_lb'] # todo: potentially confusing with the age_up in the contact matrix
-    age_ub = params.layerchars['age_ub']
-
-    custom_contacts = co.make_custom_contacts(uids, n_contacts, pop_size, ages, custom_lkeys, cluster_types, pop_proportion, age_lb, age_ub)
-    contacts.update(custom_contacts)
-
-    return
+    return popdict
