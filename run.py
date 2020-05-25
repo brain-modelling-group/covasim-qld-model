8f75af209134fac893be7e6697879d6ca04ae857
samhainsworth <SamuelHainsworth@users.noreply.github.com>
2020-05-25 14:43:31 +1000

    Manual updates to pars & metapars in code

--- run.py
+++ run.py
@@ -7,4 +7,8 @@ root = os.path.dirname(os.path.abspath(__file__))
 # create data object
 setting = 'victoria'
 file_name = 'vic-data'
-setup.setup(root, file_name, setting)
+
+custom_pars = {'beta': 0.11, 'n_days': 60}
+metapars = {'noise': 0, 'n_runs': 10}
+
+setup.setup(root, file_name, setting, custom_pars=custom_pars, metapars=metapars)
--- setup.py
+++ setup.py
@@ -15,9 +15,11 @@ def get_popdict(params):
     return popdict
 
 
-def setup(root, file_name, setting, epidata_loc='epi_data'):
+def setup(root, file_name, setting, custom_pars=None, metapars=None, epidata_loc='epi_data'):
 
     params = parameters.setup_params(root, file_name, setting, epidata_loc)
+    params.update_pars(custom_pars)
+    params.update_metapars(metapars)
 
     popdict = get_popdict(params)
 
