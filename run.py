843d5f009f464f3b176925ccf124846ebe599f23
samhainsworth <SamuelHainsworth@users.noreply.github.com>
2020-05-27 18:06:25 +1000

    Added name eq. main, few extra func

--- run.py
+++ run.py
@@ -1,21 +1,39 @@
 import os
 import setup
+import numpy as np
 
-# file path
-root = os.path.dirname(os.path.abspath(__file__))
 
-# create data object
-setting = 'victoria'
-file_name = 'vic-data'
-epidata_name = 'data/vic-epi-data.xlsx'
+if __name__ == '__main__':
+    np.random.seed(1) # Help with testing
+    # file path
+    root = os.path.dirname(os.path.abspath(__file__))
 
-pars = {'beta': 0.11, 'n_days': 60}
-metapars = {'n_runs': 10,
-            'noise': 0}
+    # data location
+    setting = 'victoria'
+    file_name = 'vic-data'
+    epidata_name = 'data/vic-epi-data.xlsx'
 
-setup.setup(root=root,
-            databook_name=file_name,
-            epidata_name=epidata_name,
-            setting=setting,
-            pars=pars,
-            metapars=metapars)
+    # parameters & meta-parameters
+    pars = {'pop_size': 100,
+            'beta': 0.11,
+            'n_days': 60}
+    metapars = {'n_runs': 10,
+                'noise': 0,
+                'verbose': 1,
+                'rand_seed': 1}
+
+    # popdict settings
+    load_popdict = False
+    save_popdict = True
+    popfile = 'data/popfile_v2.obj'
+
+    scens = setup.setup(root=root,
+                        databook_name=file_name,
+                        epidata_name=epidata_name,
+                        setting=setting,
+                        pars=pars,
+                        metapars=metapars,
+                        load_popdict=load_popdict,
+                        save_popdict=save_popdict,
+                        popfile=popfile)
+    scens.run()
