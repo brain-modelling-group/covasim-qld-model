7274a545d9565620799a4f577aa503b4147c6b9d
samhainsworth <SamuelHainsworth@users.noreply.github.com>
2020-06-04 13:41:26 +1000

    Removed re-declared function

--- contacts.py
+++ contacts.py
@@ -94,45 +94,6 @@ def make_wcontacts(uids, ages, w_contacts):
     return work_co
 
 
-def random_contacts(include, mean_contacts_per_person, array_output:bool=False):
-    """
-    Sample random contacts
-
-    Note that random contacts are directed and asymmetric (e.g. Person 1 can transmit to Person 2 but not vice-versa).
-
-    The `include` argument allows a subset of the population to be selected
-
-    Note that a person can contact themselves, and can contact the same person multiple times
-
-    Args:
-        include: Boolean array with length equal to population size, containing True if the person is eligible for contacts
-        mean_contacts_per_person: Mean number of contacts (Poisson distribution)
-        array_output: Return contacts as arrays or as dicts
-    Returns:
-        If array_output=False, return a contacts dictionary {1:[2,3,4],2:[1,5,6]} with keys for source person,
-        and a values being a list of target contacts.
-
-        If array_output=True, return arrays with `source` and `target` indexes. These could be interleaved to produce an edge list
-        representation of the edges
-
-    """
-
-    include_inds = np.nonzero(include)[0].astype(cvd.default_int) # These are the indexes (person IDs) of people in the layer
-    n_people = len(include_inds)
-    n_contacts = n_people*mean_contacts_per_person
-    source = include_inds[np.array(cvu.choose_r(max_n=n_people, n=n_contacts))]  # Choose with replacement
-    target = include_inds[np.array(cvu.choose_r(max_n=n_people, n=n_contacts))]
-
-    if array_output:
-        return source, target
-    else:
-        contacts = collections.defaultdict(list)
-        for s, t in zip(source, target):
-            contacts[s].append(t)
-        contacts = {p: contacts[p] if p in contacts else list() for p in include_inds}
-        return contacts
-
-
 def make_custom_contacts(uids, n_contacts, pop_size, ages, custom_lkeys, cluster_types, pop_proportion, age_lb, age_ub):
     contacts = {}
     for layer_key in custom_lkeys:
