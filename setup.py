a01050bc055c34fa1d60324e05f0c0e1cc8bb92b
samhainsworth <SamuelHainsworth@users.noreply.github.com>
2020-06-05 11:09:01 +1000

    setup scenarios (almost done)

--- setup.py
+++ setup.py
@@ -5,15 +5,13 @@ import parameters
 import scenarios
 import utils
 
+
 def setup_scens(locations,
                 db_name,
                 epi_name='url',
                 policy_change=None,
                 user_pars=None,
-                metapars=None,
-                load_popdict=False,
-                save_popdict=False,
-                pop_name='popfile.obj'):
+                metapars=None):
 
     # for reproducible results
     utils.set_rand_seed(metapars)
@@ -21,35 +19,40 @@ def setup_scens(locations,
     # return data relevant to each specified location in "locations"
     all_data = data.read_data(locations=locations,
                               db_name=db_name,
-                              epi_name=epi_name,
-                              pop_name=pop_name)
+                              epi_name=epi_name)
 
     all_scens = {}
     for location in locations:
         loc_data = all_data[location]
+        loc_epidata = all_data[location]
+        keys = all_data[location]
         loc_pars = user_pars[location]
 
+        # setup parameters object for this simulation
         params = parameters.setup_params(location=location,
                                          loc_data=loc_data,
                                          metapars=metapars,
-                                         user_pars=loc_pars)
+                                         user_pars=loc_pars,
+                                         keys=keys)
 
-        # popdict = co.get_popdict(params=params,
-        #                          popfile=pop_path,
-        #                          load_popdict=load_popdict,
-        #                          save_popdict=save_popdict)
-        #
-        # # setup simulation
-        # sim = cv.Sim(pars=params.pars,
-        #              datafile=epi_path,
-        #              popfile=pop_path,
-        #              pop_size=params.pars['pop_size'],
-        #              save_pop=False,
-        #              load_pop=True)
-        # sim.initialize()
-        #
-        # # setup scenarios
-        # scens = scenarios.define_scenarios(policy_change=policy_change, params=params, popdict=popdict)
-        # scens = cv.Scenarios(sim=sim, metapars=metapars, scenarios=scens) # TODO: want to pass basepars here/correct ones to pass?
-        #
-        # all_scens[location] = scens
+        popdict = co.get_popdict(params=params)
+
+        # setup simulation for this location
+        sim = cv.Sim(pars=params.pars,
+                     datafile=loc_epidata,
+                     popfile=popdict,
+                     pop_size=params.pars['pop_size'],
+                     load_pop=True,
+                     save_pop=False)
+        sim.initialize()
+
+        # setup scenarios for this location
+        scens = scenarios.define_scenarios(policy_change=policy_change,
+                                           params=params,
+                                           popdict=popdict)
+        scens = cv.Scenarios(sim=sim,
+                             metapars=metapars,
+                             scenarios=scens)
+        all_scens[location] = scens
+
+    return all_scens
