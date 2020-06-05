c21b3c7a755273d5924329668b4763bad858e73f
samhainsworth <SamuelHainsworth@users.noreply.github.com>
2020-06-06 07:48:24 +1000

    Another user message

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ def setup_scens(locations,
 
     all_scens = {}
     for location in locations:
+        print(f'\n Creating scenarios for "{location}"...')
         loc_data = all_data[location]
         loc_epidata = all_data[location]['epidata']
         keys = all_data[location]
