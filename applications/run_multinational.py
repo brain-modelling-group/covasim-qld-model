bae71ffdea116a600b7d589815700ce559446666
samhainsworth <SamuelHainsworth@users.noreply.github.com>
2020-06-09 15:49:56 +1000

    Updated the run script

--- applications/run_multinational.py
+++ applications/run_multinational.py
@@ -1,31 +1,39 @@
 import user_interface as ui
 
-# international
+# the list of locations for this analysis
 locations = ['Australia', 'New Zealand']
 db_name = 'input_data'
 
+# country-specific parameters
 user_pars = {'Australia': {'pop_size': int(2e4),
-                          'beta': 0.05,
-                          'n_days': 60},
+                           'beta': 0.05,
+                           'n_days': 60},
             'New Zealand': {'pop_size': int(2e4),
                             'beta': 0.05,
                             'n_days': 60}}
 
+# the metapars for all countries and scenarios
 metapars = {'n_runs': 2,
             'noise': 0,
             'verbose': 1,
             'rand_seed': 1}
 
+# if required, change the each policy beta, date_implemented & date_ended
+policy_vals = {'Australia': {'lockdown': {'beta': .5}}}
+
+# the policies to change during scenario runs
 policy_change = {'Australia': {'relax lockdown': {'replace': (['lockdown'], [['lockdown_relax']], [[20]])}},
                  'New Zealand': {'relax lockdown': {'replace': (['lockdown'], [['lockdown_relax']], [[20]])}}}
 
-
-
+# set up the scenarios
 scens = ui.setup_scens(locations=locations,
                        db_name=db_name,
                        epi_name='url',
                        policy_change=policy_change,
                        user_pars=user_pars,
-                       metapars=metapars)
-
+                       metapars=metapars,
+                       policy_vals=policy_vals)
+# run the scenarios
 scens = ui.run_scens(scens)
+
+ui.policy_plot(scens['Australia'])
