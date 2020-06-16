8e59c241bc50e22752823396d6890d238ed63eb9
fhou1 <64887646+fhou1@users.noreply.github.com>
2020-06-16 11:42:56 +1000

    update brazil

--- applications/run_manaus.py
+++ applications/run_manaus.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     # country-specific parameters
     user_pars = {'Manaus': {'pop_size': int(10e4),
                                'beta': 0.05,
-                               'n_days': 200}}
+                               'n_days': 365}}
 
     # the metapars for all countries and scenarios
     metapars = {'n_runs': 3,
@@ -20,23 +20,19 @@ if __name__ == "__main__":
 
     # if required, change the each policy beta, date_implemented & date_ended
     policy_vals = {'Manaus': {'lockdown_1': {'beta': 0.2},
-                              'lockdown_2': {'beta': 0.1}, 
+                              'lockdown_2': {'beta': 0.15}, 
                               'lockdown_3': {'beta': 0.1},
                               'relax_1': {'beta': 0.3}}}
 
     # Note that lockdown 3 comes into effect the first time at day 90
     
     # the policies to change during scenario runs
-    # policy_change = {'Manaus': {'24 week relax': {'replace': (['lockdown_3'], [['relax_1']], 
-    #                                                     [[272]])},
     policy_change = {'Manaus': {'12 week relax': {'replace': (['lockdown_3'], [['relax_1']], 
                                                         [[188]])},
                                 '8 week relax': {'replace': (['lockdown_3'], [['relax_1']], 
-                                                        [[160]])}}}
-                                # '12 week relax': {'replace': (['lockdown_3'], [['lockdown_2']], 
-                                #                         [[174]])},
-                                # '24 week relax': {'replace': (['lockdown_3'], [['lockdown_2']], 
-                                #                         [[258]])}}}
+                                                        [[160]])}}
+                     }
+                     
     # set up the scenarios
     scens = ui.setup_scens(locations=locations,
                            db_name=db_name,
@@ -53,6 +49,11 @@ if __name__ == "__main__":
     ui.policy_plot(scens['Manaus'], to_plot={'Cumulative Diagnosis (Manaus)': 'cum_diagnoses'})
     ui.policy_plot(scens['Manaus'], to_plot={'Cumulative Infections (Manaus)': 'cum_infections'})
     ui.policy_plot(scens['Manaus'], to_plot={'New Daily Infections (Manaus)': 'new_infections'})
-    # number of infections that occurred between day 100 and day 150
-    infections = sum(scens['Manaus'].results['new_infections']['baseline']['best'][100:150])
-    print(infections)
+    
+    # number of infections that occurred between November and February
+    infections2 = sum(scens['Manaus'].results['new_infections']['12 week relax']['best'][234:354])
+    print(infections2)
+    infections3 = sum(scens['Manaus'].results['new_infections']['8 week relax']['best'][234:354])
+    print(infections3)
+    
+    
