2da8e1547df4b4d214a0696fedb056d1f333854e
annapal <33274190+annapal@users.noreply.github.com>
2020-06-16 20:52:04 +1000

    update south africa and illinois

--- applications/south_africa/plot_sa.py
+++ applications/south_africa/plot_sa.py
@@ -1,14 +1,29 @@
 import user_interface as ui
 import pickle
+import os
+import utils
 
-locations = ['FS']
+dirname = os.path.dirname(__file__)
+locations = ['NW']
 filehandler = open('runs/' + locations[0] + '.obj', 'rb')
 scens = pickle.load(filehandler)
+scens['verbose'] = True
 
-infections = [sum(scens[locations[0]].results['new_infections']['Restrictions re-introduced Jul-Oct']['best'][231:323]),  # don't forget to change these dates for KZN
-              sum(scens[locations[0]].results['new_infections']['Restrictions re-introduced Sep-Nov']['best'][231:323])]
-print(infections)
+utils.policy_plot2(scens, plot_ints=False, do_save=True, do_show=True, fig_path=dirname + '/' + locations[0] + '.png',
+                  interval=30,
+                  fig_args=dict(figsize=(10, 5), dpi=100),
+                  font_size=11,
+                  #y_lim={'new_infections': 500},
+                  legend_args={'loc': 'upper center', 'bbox_to_anchor': (0.5, -0.1)},
+                  axis_args={'left': 0.1, 'right': 0.9, 'bottom': 0.2},
+                  fill_args={'alpha': 0.0},
+                  to_plot=['new_infections'])
 
-# ui.policy_plot(scens, outcomes_toplot={'Cumulative Deaths': 'cum_deaths'}, scens_toplot={locations[0]: ['baseline']}, plot_ints=False)
-ui.policy_plot(scens, outcomes_toplot={'New Infections': 'new_infections'}, scens_toplot={locations[0]: ['Restrictions re-introduced Jul-Oct',
-                                                                                                         'Restrictions re-introduced Sep-Nov']}, plot_ints=False)
+# infections = [sum(scens[locations[0]].results['new_infections']['Restrictions re-introduced Jul-Oct']['best'][219:311]),  # don't forget to change these dates for KZN
+#               sum(scens[locations[0]].results['new_infections']['Restrictions re-introduced Sep-Nov']['best'][219:311])]
+# print(infections)
+#
+# # ui.policy_plot(scens, outcomes_toplot={'Cumulative Deaths': 'cum_deaths'}, scens_toplot={locations[0]: ['baseline']}, plot_ints=False)
+# ui.policy_plot(scens, outcomes_toplot={'New Infections': 'new_infections'}, scens_toplot={locations[0]: ['Restrictions re-introduced Jul-Oct',
+#                                                                                                          'Restrictions re-introduced Sep-Nov']},
+#                plot_ints=False, do_save=True, name='FS')
--- applications/us_states/run_il.py
+++ applications/us_states/run_il.py
@@ -1,5 +1,7 @@
 import user_interface as ui
-
+import utils
+import os
+import pickle
 
 if __name__ == "__main__":
     # the list of locations for this analysis
@@ -10,20 +12,22 @@ if __name__ == "__main__":
 
     # country-specific parameters
     user_pars = {'IL': {'pop_size': int(10e4),
-                               'beta': 0.4,
-                               'n_days': 80}}
+                               'beta': 0.0284,
+                               'n_days': 365}}
 
     # the metapars for all countries and scenarios
-    metapars = {'n_runs': 2,
+    metapars = {'n_runs': 1,
                 'noise': 0,
                 'verbose': 1,
                 'rand_seed': 1}
 
     # if required, change the each policy beta, date_implemented & date_ended
-    policy_vals = {'IL': {'lockdown': {'beta': .5}}}
+    policy_vals = {'IL': {'lockdown': {'beta': .15},
+                          'lockdown_relax': {'beta': 1.}}}
 
     # the policies to change during scenario runs
-    policy_change = {'IL': {'relax lockdown': {'replace': (['lockdown'], [['lockdown_relax']], [[20]])}}}
+    policy_change = {'IL': {'Restrictions re-introduced Jul-Oct': {'replace': (['lockdown_relax'], [['lockdown']], [[80 + 8 * 7, 80 + 8 * 7 + 10 * 7]])},
+                            'Restrictions re-introduced Sep-Nov': {'replace': (['lockdown_relax'], [['lockdown']], [[80 + 16 * 7, 80 + 16 * 7 + 10 * 7]])}}}
 
     # set up the scenarios
     scens = ui.setup_scens(locations=locations,
@@ -37,6 +41,23 @@ if __name__ == "__main__":
     scens = ui.run_scens(scens)
 
     # plot
-    ui.policy_plot(scens, outcomes_toplot={'Cumulative Deaths': 'cum_deaths'},
-                   scens_toplot={locations[0]: ['baseline']}, plot_ints=False)
-    ui.policy_plot(scens, outcomes_toplot={'New Infections': 'new_infections'}, scens_toplot={locations[0]: ['baseline', 'relax lockdown']}, plot_ints=False)
+    scens['verbose'] = True
+
+    dirname = os.path.dirname(__file__)
+    utils.policy_plot2(scens, plot_ints=False, do_save=True, do_show=True,
+                       fig_path=dirname + '/' + locations[0] + '.png',
+                       interval=30,
+                       fig_args=dict(figsize=(10, 5), dpi=100),
+                       font_size=11,
+                       # y_lim={'new_infections': 500},
+                       legend_args={'loc': 'upper center', 'bbox_to_anchor': (0.5, -0.1)},
+                       axis_args={'left': 0.1, 'right': 0.9, 'bottom': 0.2},
+                       fill_args={'alpha': 0.0},
+                       to_plot=['new_infections'])
+
+    infections = [sum(scens[locations[0]].results['new_infections']['Restrictions re-introduced Jul-Oct']['best'][236:356]),  # don't forget to change these dates for KZN
+                  sum(scens[locations[0]].results['new_infections']['Restrictions re-introduced Sep-Nov']['best'][236:356])]
+    print(infections)
+
+    filehandler = open('runs/' + locations[0] + '.obj', 'wb')
+    pickle.dump(scens, filehandler)
