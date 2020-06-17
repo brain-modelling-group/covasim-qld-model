6c3ac439a7d47112229a82e99ef3a5919a0ed1ea
Tom Walsh <tom.walsh@burnet.edu.au>
2020-06-17 17:19:20 +1000

    minesota & arizona updates

--- applications/us_states/run_arizona.py
+++ applications/us_states/run_arizona.py
@@ -5,16 +5,16 @@ import utils
 
 if __name__ == "__main__":
     # the list of locations for this analysis
-    locations = ['Minesota']
+    locations = ['Arizona']
     # the name of the databook
     db_name = 'input_data_min'
     epi_name = 'epi_data_min'
 
     # country-specific parameters
-    user_pars = {'Minesota': {'pop_size': int(7.5e4),
-                               'beta': 0.1,
-                               'n_days': 365,
-                          'pop_infected': 300}
+    user_pars = {'Arizona': {'pop_size': int(7.5e4),
+                               'beta': 0.035,
+                               'n_days': 100,
+                          'pop_infected': 100}
 
                  }
 
@@ -25,16 +25,15 @@ if __name__ == "__main__":
                 'rand_seed': 1}
 
     # if required, change the each policy beta, date_implemented & date_ended
-    policy_vals = {'Minesota': {'lockdown_1':{'beta':0.53},
-                                'lockdown_2': {'beta': 0.27},
-                                'relax_1': {'beta': 0.25},
+    policy_vals = {'Minesota': {'lockdown_1':{'beta': 0.5},
+                                'lockdown_2': {'beta': 0.25},
+                                'relax_1': {'beta': 0.1},
                                 'relax_2': {'beta': 0.2},
                    }}
     # the policies to change during scenario runs
 
-    policy_change = {'Minesota': {'Lockdown relaxed in August': {'replace': (['relax_1'], [['relax_2']], [[365]])},
-                                  'Lockdown increased in October': {'replace':(['relax_1'], [['relax_2']],[[180]])}
-                                  }}
+    policy_change = {'Arizona': {'Lockdown relax in August':{'replace': (['relax_1'], [['relax_2']], [[161]])}
+                                                            }}
     # set up the scenarios
     scens = ui.setup_scens(locations=locations,
                            db_name=db_name,
@@ -53,8 +52,8 @@ if __name__ == "__main__":
     #PLOT
     dirname = os.path.dirname(__file__)
     scens['verbose'] = True
-    utils.policy_plot2(scens, plot_ints=False, do_save=True, do_show=True,
-                       fig_path=dirname + '/Minesota_in_prog_infections' + '.png',
+    utils.policy_plot2(scens, plot_ints=False, do_save=False, do_show=True,
+                       fig_path=dirname + '/Arizona_in_prog_infections' + '.png',
                        interval=30,
                        fig_args=dict(figsize=(10, 5), dpi=100),
                        font_size=11,
@@ -67,7 +66,7 @@ if __name__ == "__main__":
     dirname = os.path.dirname(__file__)
     scens['verbose'] = True
     utils.policy_plot2(scens, plot_ints=False, do_save=False, do_show=True,
-                       fig_path=dirname + '/Minesota_in_prog_deaths_calibrate' + '.png',
+                       fig_path=dirname + '/Arizona_in_prog_deaths' + '.png',
                        interval=30,
                        fig_args=dict(figsize=(10, 5), dpi=100),
                        font_size=11,
@@ -76,9 +75,4 @@ if __name__ == "__main__":
                        axis_args={'left': 0.1, 'right': 0.9, 'bottom': 0.2},
                        fill_args={'alpha': 0.0},
                        to_plot=['cum_deaths'])
-
-    August_release = sum(scens['Minesota'].results['new_infections']['Lockdown relaxed in August']['best'][243:363])
-    print('restrictions lifted in August =', August_release)
-    November_release = sum(scens['Minesota'].results['new_infections']['Lockdown increased in October']['best'][243:363])
-    print('Lockdown increased October  =', November_release)
 
