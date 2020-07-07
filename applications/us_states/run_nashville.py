7dc4746aed0a52d024fef14c79bc5c1e3ed8d923
Dominic Delport <Dominic.Delport@burnet.edu.au>
2020-07-07 10:39:00 +1000

    Finally got Nashville sorted

--- applications/us_states/run_nashville.py
+++ applications/us_states/run_nashville.py
@@ -42,16 +42,13 @@ if __name__ == "__main__":
                                     'policies': {'relax4': {'beta': 0.85}}},
 
                                'Implement and maintain strict lockdown':
-                                   {'replace': (['lockdown4'], [['lockdown5']], [[132]]),
-                                    'policies': {'lockdown5': {'beta': 0.2}}},
+                                   {'replace': (['lockdown4'], [['lockdown5']], [[132]])},
 
-                               'Implement weaker lockdown':
-                                   {'replace': (['lockdown4'], [['lockdown5']], [[132]]),
-                                    'policies': {'lockdown5': {'beta': 0.4}}},
+                               'Implement strict lockdown and relax on August 15':
+                                   {'replace': (['lockdown4', 'lockdown5'], [['lockdown5'], ['lockdown6']], [[132], [172]])},
 
                                'No changes to current lockdown restrictions':
-                                   {'replace': (['lockdown4'], [['lockdown4']], [[500]]),
-                                    'policies': {'lockdown4': {'beta': 0.2}}},
+                                   {'replace': (['lockdown4'], [['lockdown4']], [[500]])},
                                }}
 
 
@@ -72,7 +69,7 @@ if __name__ == "__main__":
     new_no_release_SO = sum(
         scens['scenarios']['Nashville'].results['new_infections']['No changes to current lockdown restrictions']['best'][203:246]) * 100 * 30 / 42 / pop
     new_lockdownrelease_SO = sum(
-        scens['scenarios']['Nashville'].results['new_infections']['Implement weaker lockdown']['best'][203:246]) * 100 * 30 / 42 / pop
+        scens['scenarios']['Nashville'].results['new_infections']['Implement strict lockdown and relax on August 15']['best'][203:246]) * 100 * 30 / 42 / pop
     new_lockdownmaintain_SO = sum(
         scens['scenarios']['Nashville'].results['new_infections']['Implement and maintain strict lockdown']['best'][203:246]) * 100 * 30 / 42 / pop
     new_september_moderaterelease_SO = sum(
@@ -82,7 +79,7 @@ if __name__ == "__main__":
     diag_no_release_SO = sum(
         scens['scenarios']['Nashville'].results['new_diagnoses']['No changes to current lockdown restrictions']['best'][203:246]) * 100 * 30 / 42 / pop
     diag_lockdownrelease_SO = sum(
-        scens['scenarios']['Nashville'].results['new_diagnoses']['Implement weaker lockdown']['best'][203:246]) * 100 * 30 / 42 / pop
+        scens['scenarios']['Nashville'].results['new_diagnoses']['Implement strict lockdown and relax on August 15']['best'][203:246]) * 100 * 30 / 42 / pop
     diag_lockdownmaintain_SO = sum(
         scens['scenarios']['Nashville'].results['new_diagnoses']['Implement and maintain strict lockdown']['best'][203:246]) * 100 * 30 / 42 / pop
     diag_september_moderaterelease_SO = sum(
@@ -92,7 +89,7 @@ if __name__ == "__main__":
     cum_no_release_SO = \
     scens['scenarios']['Nashville'].results['cum_infections']['No changes to current lockdown restrictions']['best'][246] / pop
     cum_lockdownrelease_SO = \
-    scens['scenarios']['Nashville'].results['cum_infections']['Implement weaker lockdown']['best'][246] / pop
+    scens['scenarios']['Nashville'].results['cum_infections']['Implement strict lockdown and relax on August 15']['best'][246] / pop
     cum_lockdownmaintain_SO = \
     scens['scenarios']['Nashville'].results['cum_infections']['Implement and maintain strict lockdown']['best'][246] / pop
     cum_september_moderaterelease_SO = \
@@ -103,7 +100,7 @@ if __name__ == "__main__":
     new_no_release_ND = sum(
         scens['scenarios']['Nashville'].results['new_infections']['No changes to current lockdown restrictions']['best'][264:307]) * 100 * 30 / 42 / pop
     new_lockdownrelease_ND = sum(
-        scens['scenarios']['Nashville'].results['new_infections']['Implement weaker lockdown']['best'][264:307]) * 100 * 30 / 42 / pop
+        scens['scenarios']['Nashville'].results['new_infections']['Implement strict lockdown and relax on August 15']['best'][264:307]) * 100 * 30 / 42 / pop
     new_lockdownmaintain_ND = sum(
         scens['scenarios']['Nashville'].results['new_infections']['Implement and maintain strict lockdown']['best'][264:307]) * 100 * 30 / 42 / pop
     new_september_moderaterelease_ND = sum(
@@ -113,7 +110,7 @@ if __name__ == "__main__":
     diag_no_release_ND = sum(
         scens['scenarios']['Nashville'].results['new_diagnoses']['No changes to current lockdown restrictions']['best'][264:307]) * 100 * 30 / 42 / pop
     diag_lockdownrelease_ND = sum(
-        scens['scenarios']['Nashville'].results['new_diagnoses']['Implement weaker lockdown']['best'][264:307]) * 100 * 30 / 42 / pop
+        scens['scenarios']['Nashville'].results['new_diagnoses']['Implement strict lockdown and relax on August 15']['best'][264:307]) * 100 * 30 / 42 / pop
     diag_lockdownmaintain_ND = sum(
         scens['scenarios']['Nashville'].results['new_diagnoses']['Implement and maintain strict lockdown']['best'][264:307]) * 100 * 30 / 42 / pop
     diag_september_moderaterelease_ND = sum(
@@ -123,7 +120,7 @@ if __name__ == "__main__":
     cum_no_release_ND = \
     scens['scenarios']['Nashville'].results['cum_infections']['No changes to current lockdown restrictions']['best'][307] / pop
     cum_lockdownrelease_ND = \
-    scens['scenarios']['Nashville'].results['cum_infections']['Implement weaker lockdown']['best'][307] / pop
+    scens['scenarios']['Nashville'].results['cum_infections']['Implement strict lockdown and relax on August 15']['best'][307] / pop
     cum_lockdownmaintain_ND = \
     scens['scenarios']['Nashville'].results['cum_infections']['Implement and maintain strict lockdown']['best'][307] / pop
     cum_september_moderaterelease_ND = \
@@ -133,42 +130,42 @@ if __name__ == "__main__":
 
     with open('Nashville_projections.txt', 'w') as f:
         print('30 day incidence for Sept-Oct: No changes to current lockdown restrictions =', new_no_release_SO, file=f)
-        print('30 day incidence for Sept-Oct: Implement weaker lockdown =', new_lockdownrelease_SO, file=f)
+        print('30 day incidence for Sept-Oct: Implement strict lockdown and relax on August 15 =', new_lockdownrelease_SO, file=f)
         print('30 day incidence for Sept-Oct: Implement and maintain strict lockdown =', new_lockdownmaintain_SO,
               file=f)
         print('30 day incidence for Sept-Oct: Moderate easing of current restrictions on September 15 =', new_september_moderaterelease_SO, file=f)
         print('30 day incidence for Sept-Oct: Moderate easing of current restrictions on on August 15 =', new_august_moderaterelease_SO,
               file=f)
         print('30 day detected cases for Sept-Oct: No changes to current lockdown restrictions =', diag_no_release_SO, file=f)
-        print('30 day detected cases for Sept-Oct: Implement weaker lockdown =', diag_lockdownrelease_SO, file=f)
+        print('30 day detected cases for Sept-Oct: Implement strict lockdown and relax on August 15 =', diag_lockdownrelease_SO, file=f)
         print('30 day detected cases for Sept-Oct: Implement and maintain strict lockdown =', diag_lockdownmaintain_SO,
               file=f)
         print('30 day detected cases for Sept-Oct: Moderate easing of current restrictions on September 15 =', diag_september_moderaterelease_SO, file=f)
         print('30 day detected cases for Sept-Oct: Moderate easing of current restrictions on on August 15 =', diag_august_moderaterelease_SO,
               file=f)
         print('Cumulative infections for Sept-Oct: No changes to current lockdown restrictions =', cum_no_release_SO, file=f)
-        print('Cumulative infections for Sept-Oct: Implement weaker lockdown =', cum_lockdownrelease_SO, file=f)
+        print('Cumulative infections for Sept-Oct: Implement strict lockdown and relax on August 15 =', cum_lockdownrelease_SO, file=f)
         print('Cumulative infections for Sept-Oct: Implement and maintain strict lockdown =', cum_lockdownmaintain_SO,
               file=f)
         print('Cumulative infections for Sept-Oct: Moderate easing of current restrictions on September 15 =', cum_september_moderaterelease_SO, file=f)
         print('Cumulative infections for Sept-Oct: Moderate easing of current restrictions on on August 15 =', cum_august_moderatelrelease_SO,
               file=f)
         print('30 day incidence for Nov-Dec: No changes to current lockdown restrictions =', new_no_release_ND, file=f)
-        print('30 day incidence for Nov-Dec: Implement weaker lockdown =', new_lockdownrelease_ND, file=f)
+        print('30 day incidence for Nov-Dec: Implement strict lockdown and relax on August 15 =', new_lockdownrelease_ND, file=f)
         print('30 day incidence for Nov-Dec: Implement and maintain strict lockdown =', new_lockdownmaintain_ND,
               file=f)
         print('30 day incidence for Nov-Dec: Moderate easing of current restrictions on September 15 =', new_september_moderaterelease_ND, file=f)
         print('30 day incidence for Nov-Dec: Moderate easing of current restrictions on on August 15 =', new_august_moderaterelease_ND,
               file=f)
         print('30 day detected cases for Nov-Dec: No changes to current lockdown restrictions =', diag_no_release_ND, file=f)
-        print('30 day detected cases for Nov-Dec: Implement weaker lockdown =', diag_lockdownrelease_ND, file=f)
+        print('30 day detected cases for Nov-Dec: Implement strict lockdown and relax on August 15 =', diag_lockdownrelease_ND, file=f)
         print('30 day detected cases for Nov-Dec: Implement and maintain strict lockdown =', diag_lockdownmaintain_ND,
               file=f)
         print('30 day detected cases for Nov-Dec: Moderate easing of current restrictions on September 15 =', diag_september_moderaterelease_ND, file=f)
         print('30 day detected cases for Nov-Dec: Moderate easing of current restrictions on on August 15 =', diag_august_moderaterelease_ND,
               file=f)
         print('Cumulative infections for Nov-Dec: No changes to current lockdown restrictions =', cum_no_release_ND, file=f)
-        print('Cumulative infections for Nov-Dec: Implement weaker lockdown =', cum_lockdownrelease_ND, file=f)
+        print('Cumulative infections for Nov-Dec: Implement strict lockdown and relax on August 15 =', cum_lockdownrelease_ND, file=f)
         print('Cumulative infections for Nov-Dec: Implement and maintain strict lockdown =', cum_lockdownmaintain_ND,
               file=f)
         print('Cumulative infections for Nov-Dec: Moderate easing of current restrictions on September 15 =', cum_september_moderaterelease_ND, file=f)
@@ -191,11 +188,11 @@ if __name__ == "__main__":
     # plot cumulative infections to see if all the population gets infected
     utils.policy_plot2(scens, plot_ints=False, do_save=True, do_show=True,
                        fig_path=dirname + '/figs/Nashville-projections' + '.png',
-                       interval=30, n_cols=2,
+                       interval=30, n_cols=1,
                        fig_args=dict(figsize=(10, 5), dpi=100),
                        font_size=11,
                        # y_lim={'new_infections': 500},
-                       #legend_args={'loc': 'lower right', 'bbox_to_anchor': (1.0, -1.6)},
-                       axis_args={'left': 0.1, 'wspace': 0.2, 'right': 0.99, 'hspace': 0.4, 'bottom': 0.15},
-                       fill_args={'alpha': 0.3},
-                       to_plot=['new_infections', 'cum_infections', 'cum_diagnoses'])
+                       legend_args={'loc': 'upper center', 'bbox_to_anchor': (0.5, -1.6)},
+                       axis_args={'left': 0.1, 'wspace': 0.2, 'right': 0.95, 'hspace': 0.4, 'bottom': 0.3},
+                       fill_args={'alpha': 0.1},
+                       to_plot=['new_infections', 'cum_infections'])
