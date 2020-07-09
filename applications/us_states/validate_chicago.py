8817a1603f1746e2b5762be6ebd2e8bcfee6d797
annapal <33274190+annapal@users.noreply.github.com>
2020-07-09 17:36:55 +1000

    Revert "Merge branch 'master' of https://github.com/optimamodel/covasim-australia"
    This reverts commit 068b88dfea2a18d6cc41864757ece017c93cd8fc, reversing
    changes made to 73246d7c5e130fe015ca0ba7918377c6cc8407fa.

--- applications/us_states/validate_chicago.py
+++ applications/us_states/validate_chicago.py
@@ -2,7 +2,6 @@ import user_interface as ui
 import utils
 import os
 dirname = os.path.dirname(__file__)
-import xlsxwriter
 
 if __name__ == "__main__":
     # the list of locations for this analysis
@@ -18,13 +17,13 @@ if __name__ == "__main__":
     # country-specific parameters
     user_pars = {'Chicago': {'pop_size': int(10e4),
                                'beta': 0.139,
-                               'n_days': 126,
-                               # 'symp_test': 5.0,
+                               'n_days': 130,
+                               'symp_test': 5.0,
                                'calibration_end': '2020-05-15'}}
 
     # the metapars for all countries and scenarios
-    metapars = {'n_runs': 1,
-                'noise': 0,
+    metapars = {'n_runs': 10,
+                'noise': 0.03,
                 'verbose': 1,
                 'rand_seed': 1}
     
@@ -54,33 +53,6 @@ if __name__ == "__main__":
               font_size=11,
               #y_lim={'new_infections': 500},
               legend_args={'loc': 'upper center', 'bbox_to_anchor': (1.0, -1.6)},
-              axis_args={'left': 0.1, 'wspace': 0.2,'right': 0.99, 'hspace': 0.4,'bottom': 0.15},
+              axis_args={'left': 0.08, 'wspace': 0.2,'right': 0.99, 'hspace': 0.4,'bottom': 0.15},
               fill_args={'alpha': 0.3},
               to_plot=['new_infections', 'cum_infections', 'cum_diagnoses', 'cum_deaths'])
-    
-    
-    cum_diag_calib_end1 = scens['scenarios']['Chicago'].results['cum_diagnoses']['No changes to current lockdown restrictions']['best'][80]    
-    cum_diag_calib_1week = scens['scenarios']['Chicago'].results['cum_diagnoses']['No changes to current lockdown restrictions']['best'][87]    
-    cum_diag_calib_2week = scens['scenarios']['Chicago'].results['cum_diagnoses']['No changes to current lockdown restrictions']['best'][94]    
-    cum_diag_calib_end2 = scens['scenarios']['Chicago'].results['cum_diagnoses']['No changes to current lockdown restrictions']['best'][126]    
-    cum_death_calib_end1 = scens['scenarios']['Chicago'].results['cum_deaths']['No changes to current lockdown restrictions']['best'][80]    
-    cum_death_calib_1week = scens['scenarios']['Chicago'].results['cum_deaths']['No changes to current lockdown restrictions']['best'][87]    
-    cum_death_calib_2week = scens['scenarios']['Chicago'].results['cum_deaths']['No changes to current lockdown restrictions']['best'][94]    
-    cum_death_calib_end2 = scens['scenarios']['Chicago'].results['cum_deaths']['No changes to current lockdown restrictions']['best'][126]    
-        
-    workbook = xlsxwriter.Workbook('Chicago_validation.xlsx')     
-    worksheet = workbook.add_worksheet('Validation')
-    
-    validation = [['Cumulative Diagnoses (Projections)', '', '', '', 'Cumulative Diagnoses (Data)', '', '', '',
-                  'Cumulative Deaths (Projections)', '', '', '', 'Cumulative Deaths (Data)', '', '', ''],
-                  ['At end of calibration', 'After 1 week', 'After 2 weeks', 'At end of projection',
-                    'At end of calibration', 'After 1 week', 'After 2 weeks', 'At end of projection',
-                    'At end of calibration', 'After 1 week', 'After 2 weeks', 'At end of projection',
-                    'At end of calibration', 'After 1 week', 'After 2 weeks', 'At end of projection'],
-                  [int(cum_diag_calib_end1), int(cum_diag_calib_1week), int(cum_diag_calib_2week), int(cum_diag_calib_end2),
-                  '', '', '', '',
-                  int(cum_death_calib_end1), int(cum_death_calib_1week), int(cum_death_calib_2week), int(cum_death_calib_end2)]
-                  ]
-
-    worksheet.add_table('A1:P3', {'data': validation, 'header_row': False})
-    workbook.close()
--- applications/us_states/validate_new-haven.py
+++ applications/us_states/validate_new-haven.py
@@ -2,7 +2,6 @@ import user_interface as ui
 import utils
 import os
 dirname = os.path.dirname(__file__)
-import xlsxwriter
 
 if __name__ == "__main__":
     # the list of locations for this analysis
@@ -17,12 +16,13 @@ if __name__ == "__main__":
 
     # country-specific parameters
     user_pars = {'New Haven': {'pop_size': int(10e4),
-                               'beta': 0.032,
-                               'n_days': 108,
+                               'beta': 0.075,
+                               'n_days': 120,
+                               'symp_test': 100.0,
                                'calibration_end': '2020-05-15'}}
 
     # the metapars for all countries and scenarios
-    metapars = {'n_runs': 1,
+    metapars = {'n_runs': 3,
                 'noise': 0,
                 'verbose': 1,
                 'rand_seed': 1}
@@ -55,31 +55,4 @@ if __name__ == "__main__":
               legend_args={'loc': 'upper center', 'bbox_to_anchor': (1.0, -1.6)},
               axis_args={'left': 0.1, 'wspace': 0.2,'right': 0.99, 'hspace': 0.4,'bottom': 0.15},
               fill_args={'alpha': 0.3},
-              to_plot=['new_infections', 'cum_infections', 'cum_diagnoses', 'cum_deaths'])
-    
-    
-    cum_diag_calib_end1 = scens['scenarios']['New Haven'].results['cum_diagnoses']['No changes to current lockdown restrictions']['best'][62]    
-    cum_diag_calib_1week = scens['scenarios']['New Haven'].results['cum_diagnoses']['No changes to current lockdown restrictions']['best'][69]    
-    cum_diag_calib_2week = scens['scenarios']['New Haven'].results['cum_diagnoses']['No changes to current lockdown restrictions']['best'][76]    
-    cum_diag_calib_end2 = scens['scenarios']['New Haven'].results['cum_diagnoses']['No changes to current lockdown restrictions']['best'][108]    
-    cum_death_calib_end1 = scens['scenarios']['New Haven'].results['cum_deaths']['No changes to current lockdown restrictions']['best'][62]    
-    cum_death_calib_1week = scens['scenarios']['New Haven'].results['cum_deaths']['No changes to current lockdown restrictions']['best'][69]    
-    cum_death_calib_2week = scens['scenarios']['New Haven'].results['cum_deaths']['No changes to current lockdown restrictions']['best'][76]    
-    cum_death_calib_end2 = scens['scenarios']['New Haven'].results['cum_deaths']['No changes to current lockdown restrictions']['best'][108]    
-        
-    workbook = xlsxwriter.Workbook('New-Haven_validation.xlsx')     
-    worksheet = workbook.add_worksheet('Validation')
-    
-    validation = [['Cumulative Diagnoses (Projections)', '', '', '', 'Cumulative Diagnoses (Data)', '', '', '',
-                  'Cumulative Deaths (Projections)', '', '', '', 'Cumulative Deaths (Data)', '', '', ''],
-                  ['At end of calibration', 'After 1 week', 'After 2 weeks', 'At end of projection',
-                    'At end of calibration', 'After 1 week', 'After 2 weeks', 'At end of projection',
-                    'At end of calibration', 'After 1 week', 'After 2 weeks', 'At end of projection',
-                    'At end of calibration', 'After 1 week', 'After 2 weeks', 'At end of projection'],
-                  [int(cum_diag_calib_end1), int(cum_diag_calib_1week), int(cum_diag_calib_2week), int(cum_diag_calib_end2),
-                  '', '', '', '',
-                  int(cum_death_calib_end1), int(cum_death_calib_1week), int(cum_death_calib_2week), int(cum_death_calib_end2)]
-                  ]
-
-    worksheet.add_table('A1:P3', {'data': validation, 'header_row': False})
-    workbook.close()
+              to_plot=['new_infections', 'cum_infections', 'new_diagnoses', 'cum_deaths'])
