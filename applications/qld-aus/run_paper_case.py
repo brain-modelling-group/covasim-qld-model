93812bdd8d6e4612bb6643535c6aab6dde59f798
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-08-07 11:29:32 +1000

    Make example run with current code architecture

--- applications/qld-aus/run_paper_case.py
+++ applications/qld-aus/run_paper_case.py
@@ -19,20 +19,19 @@ pars = dict(pop_size = 200e3,
             pop_type = 'hybrid', 
             start_day = '2020-02-10',
             end_day = '2020-06-29') 
-trace_probs = 
-dict(h= 0.9 , s= 0.7 , w= 0.7 , c= 0.3) 
+trace_probs = dict(h= 0.9 , s= 0.7 , w= 0.7 , c= 0.3) 
 
 trace_time = dict(h= 0 , s= 1 , w= 1 , c= 3 ) 
 
-interventions = [ 17 cv.clip_edges(start_day= '2020-03-26' , end_day= None , change={ 's' : 0.0 }), # 
+interventions = [ cv.clip_edges(days= ['2020-03-26'], changes=[0.0], layers=['s']), # 
 
-cv.clip_edges(start_day= '2020-03-26' , end_day= '2020-04-10' , change={ 'w' : 0.7 , 'c' : 0.7 }), 
+cv.clip_edges(days= ['2020-03-26', '2020-04-10'],  changes= [0.7, 0.7], layers=['w', 'c']), 
 #
-cv.clip_edges(start_day= '2020-04-10' , end_day= '2020-05-05' , change={ 'w' : 0.3 , 'c' : 0.3 }), # 
+cv.clip_edges(days= ['2020-04-10', '2020-05-05'], changes= [0.3, 0.3], layers=['w', 'c']), # 
 
-cv.clip_edges(start_day= '2020-05-05' , end_day= None , change={ 'w' : 0.8 , 'c' : 0.8 }), # 
+cv.clip_edges(days= ['2020-05-05',  '2020-06-29'], changes=[0.8, 0.8], layers=['w', 'c']), # 
 
-cv.test_prob(start_day= '2020-05-20' , symp_prob= 0.10 , symp_quar_prob= 0.8 , test_delay= 2 ), 
+cv.test_prob(start_day= ['2020-05-20'] , symp_prob= 0.10 , symp_quar_prob= 0.8 , test_delay= 2 ), 
 
 cv.contact_tracing(start_day= '2020-04-20', 
                    trace_probs=trace_probs, 
