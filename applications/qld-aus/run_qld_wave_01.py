c8b0e142879b5b0d6d5a62d11a7f128d6adc4f5d
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-08-13 00:55:06 +1000

    Update layer edge-clipping

--- applications/qld-aus/run_qld_wave_01.py
+++ applications/qld-aus/run_qld_wave_01.py
@@ -20,24 +20,29 @@ import sciris as sc
 
 def make_sim(whattorun, julybetas=None, load_pop=True, popfile='qldppl.pop', datafile=None, agedatafile=None):
 
-    layers = ['H', 'S', 'W', 'C', 'church', 'pSport', 'cSport', 'entertainment', 'cafe_restaurant', 'pub_bar', 'transport', 'public_parks', 'large_events', 'social']
+    layers = ['H', 'S', 'W', 'C', 
+              'church', 'pSport', 'cSport', 
+              'entertainment', 
+              'cafe_restaurant', 
+              'pub_bar', 'transport', 
+              'public_parks', 'large_events', 'social']
 
     if whattorun == 'calibration':
         end_day = '2020-07-31'
     elif whattorun == 'scenarios':
-        end_day = '2020-08-31'
+        end_day = '2020-09-30'
         julybetas = julybetas
 
     pars = {'pop_size': 200e3,
             'pop_infected': 30,
-            'pop_scale': 10,
+            'pop_scale': 1,
             'rescale': False,
             'rand_seed': 42,
             'rel_death_prob': 0.8,
             'beta': 0.03, # Overall beta to use for calibration
                                     # H     S       W       C       church  psport  csport  ent     cafe    pub     trans   park    event   soc
             'contacts':    pd.Series([4,    21,     5,      1,      20,     40,     30,     25,     19,     30,     25,     10,     50,     6], index=layers).to_dict(),
-            'beta_layer':  pd.Series([1,    0.25,   0.3,    0.1,    0.04,   0.2,    0.1,    0.01,   0.04,   0.06,   0.16,   0.03,   0.01,   0.3], index=layers).to_dict(),
+            'beta_layer':  pd.Series([1,    0.3,    0.2,    0.1,    0.04,   0.2,    0.1,    0.01,   0.04,   0.06,   0.16,   0.03,   0.01,   0.3], index=layers).to_dict(),
             'iso_factor':  pd.Series([0.2,  0,      0,      0.1,    0,      0,      0,      0,      0,      0,      0,      0,      0,      0], index=layers).to_dict(),
             'quar_factor': pd.Series([1,    0.1,    0.1,    0.2,    0.01,   0,      0,      0,      0,      0,      0.1 ,   0,      0,      0], index=layers).to_dict(),
             'n_imports': 1, # Number of new cases to import per day -- varied over time as part of the interventions
@@ -62,25 +67,52 @@ def make_sim(whattorun, julybetas=None, load_pop=True, popfile='qldppl.pop', dat
     reopen5  = '2020-07-10' # regional travel open,
     school_dates = ['2020-05-11', '2020-05-18', '2020-05-25']
 
-    beta_ints = [cv.clip_edges(days=[initresponse,lockdown]+school_dates, changes=[0.75, 0.05, 0.8, 0.9, 1.0], layers=['S'], do_plot=False),
-                 cv.clip_edges(days=[lockdown, reopen2, reopen3, reopen4], changes=[0.5, 0.65, 0.75, 0.85], layers=['W'], do_plot=False),
-                 cv.clip_edges(days=[lockdown, reopen2, reopen4], changes=[0, 0.5, 1.0], layers=['pSport'], do_plot=False),
-                 cv.clip_edges(days=[lockdown, '2020-06-22'], changes=[0, 1.0], layers=['cSport'], do_plot=False),
+    beta_ints = [cv.clip_edges(days=[initresponse,lockdown]+school_dates, 
+                               changes=[0.75, 0.05, 0.8, 0.9, 1.0], 
+                               layers=['S'], do_plot=False),
+                 cv.clip_edges(days=[lockdown, reopen2, reopen3, reopen4], 
+                               changes=[0.3, 0.4, 0.5, 0.75], 
+                               layers=['W'], do_plot=False),
+                 cv.clip_edges(days=[lockdown, reopen2, reopen4], 
+                               changes=[0, 0.5, 1.0], 
+                               layers=['pSport'], do_plot=False),
+                 cv.clip_edges(days=[lockdown, '2020-06-30'],
+                               changes=[0, 1.0], 
+                               layers=['cSport'], do_plot=False),
 
                  cv.change_beta([initresponse, '2020-07-01'], [0.8, 0.9], do_plot=False), # Reduce overall beta to account for distancing, handwashing, etc
-                 cv.change_beta(days=[lockdown, reopen2, reopen4], changes=[1.2, 1.1, 1.], layers=['H'], do_plot=True),
+                 cv.change_beta(days=[lockdown, reopen2, reopen4], 
+                                changes=[1.2, 1.1, 1.], 
+                                layers=['H'], do_plot=True),
 
-                 cv.change_beta(days=[lockdown, reopen2], changes=[0, 0.7], layers=['church'], do_plot=False),
-                 cv.change_beta(days=[lockdown, reopen1, reopen2, reopen3, reopen4], changes=[0.0, 0.3, 0.4, 0.5, 0.7], layers=['social'], do_plot=False),
-
+                 cv.change_beta(days=[lockdown, reopen2], 
+                                changes=[0, 0.7], 
+                                layers=['church'], do_plot=False),
+                 cv.change_beta(days=[lockdown, reopen1, reopen2, reopen3, reopen4], 
+                                changes=[0.0, 0.3, 0.4, 0.5, 0.7], 
+                                layers=['social'], do_plot=False),
                  # Dynamic layers ['C', 'entertainment', 'cafe_restaurant', 'pub_bar', 'transport', 'public_parks', 'large_events']
-                 cv.change_beta(days=[lockdown], changes=[0.7], layers=['C'], do_plot=True),
-                 cv.change_beta(days=[lockdown, reopen4], changes=[0, 0.7], layers=['entertainment'], do_plot=False),
-                 cv.change_beta(days=[lockdown, reopen2], changes=[0, 0.7], layers=['cafe_restaurant'], do_plot=False),
-                 cv.change_beta(days=[lockdown, reopen3, reopen4], changes=[0, 0.5, 0.7], layers=['pub_bar'], do_plot=False),
-                 cv.change_beta(days=[lockdown, reopen2, reopen4], changes=[0.2, 0.3, 0.7], layers=['transport'], do_plot=False),
-                 cv.change_beta(days=[lockdown, reopen2, reopen4], changes=[0.4, 0.5, 0.7], layers=['public_parks'], do_plot=False),
-                 cv.change_beta(days=[lockdown, reopen4], changes=[0.0, 0.7], layers=['large_events'], do_plot=False),
+                 cv.change_beta(days=[lockdown], 
+                                changes=[0.7], 
+                                layers=['C'], do_plot=True),
+                 cv.change_beta(days=[lockdown, reopen4], 
+                                changes=[0, 0.7], 
+                                layers=['entertainment'], do_plot=False),
+                 cv.change_beta(days=[lockdown, reopen2], 
+                                changes=[0, 0.7], 
+                                layers=['cafe_restaurant'], do_plot=False),
+                 cv.change_beta(days=[lockdown, reopen3, reopen4], 
+                                changes=[0, 0.4, 0.5], 
+                                layers=['pub_bar'], do_plot=False),
+                 cv.change_beta(days=[lockdown, reopen2, reopen4], 
+                                changes=[0.2, 0.3, 0.7], 
+                                layers=['transport'], do_plot=False),
+                 cv.change_beta(days=[lockdown, reopen4, reopen5], 
+                                changes=[0.1, 0.5, 0.7], 
+                                layers=['public_parks'], do_plot=False),
+                 cv.change_beta(days=[lockdown, reopen4], 
+                                changes=[0.0, 0.7], 
+                                layers=['large_events'], do_plot=False),
                  ]
 
     if whattorun == 'scenarios':
@@ -94,21 +126,38 @@ def make_sim(whattorun, julybetas=None, load_pop=True, popfile='qldppl.pop', dat
     # Testing
     symp_prob_prelockdown = 0.05  # Limited testing pre lockdown
     symp_prob_lockdown = 0.3      # Increased testing during lockdown
-    symp_prob_postlockdown = 0.2  # Testing since lockdown
+    symp_prob_postlockdown = 0.35  # Testing since lockdown
     sim.pars['interventions'].append(cv.test_prob(start_day=0, end_day=lockdown, symp_prob=symp_prob_prelockdown, asymp_quar_prob=0.001, do_plot=False))
     sim.pars['interventions'].append(cv.test_prob(start_day=lockdown, end_day=reopen1, symp_prob=symp_prob_lockdown, asymp_quar_prob=0.001,do_plot=False))
     sim.pars['interventions'].append(cv.test_prob(start_day=reopen1, symp_prob=symp_prob_postlockdown, asymp_quar_prob=0.001,do_plot=True))
 
     # Tracing
-    trace_probs = {'H': 1, 'S': 0.95, 'W': 0.8, 'C': 0.05, 
-                   'church': 0.5, 'pSport': 0.8, 'cSport': 0.5,
-                   'entertainment': 0.01, 'cafe_restaurant': 0.01, 'pub_bar': 0.01, 
-                   'transport': 0.01, 'public_parks': 0.01, 'large_events': 0.01, 'social': 0.9}
-    trace_time = {'H': 0, 'S': 2, 'W': 2, 'C': 14, 
-                  'church': 5, 'pSport': 3, 'cSport': 3, 'entertainment': 14,
-                  'cafe_restaurant': 14, 'pub_bar': 14, 'transport': 14, 'public_parks': 14, 'large_events': 14,
+    trace_probs = {'H': 1, 'S': 0.95, 
+                   'W': 0.8, 'C': 0.05, 
+                   'church': 0.5, 
+                   'pSport': 0.8, 'cSport': 0.5,
+                   'entertainment': 0.01, 
+                   'cafe_restaurant': 0.8, 
+                   'pub_bar': 0.8, 
+                   'transport': 0.01, 
+                   'public_parks': 0.01, 
+                   'large_events': 0.01, 
+                   'social': 0.9}
+    trace_time = {'H': 0, 'S': 2, 
+                  'W': 2, 'C': 14, 
+                  'church': 5, 
+                  'pSport': 3, 
+                  'cSport': 3, 
+                  'entertainment': 14,
+                  'cafe_restaurant': 4, 
+                  'pub_bar': 4, 
+                  'transport': 14, 
+                  'public_parks': 14, 
+                  'large_events': 14,
                   'social': 3}
-    sim.pars['interventions'].append(cv.contact_tracing(trace_probs=trace_probs, trace_time=trace_time, start_day=0, do_plot=False))
+    sim.pars['interventions'].append(cv.contact_tracing(trace_probs=trace_probs, 
+                                                        trace_time=trace_time, 
+                                                        start_day=0, do_plot=False))
 
     # Close borders, then open them again to account for Victorian imports and leaky quarantine
     sim.pars['interventions'].append(cv.dynamic_pars({'n_imports': {'days': [115], 'vals': [3]}}, do_plot=False))
@@ -123,11 +172,10 @@ def make_sim(whattorun, julybetas=None, load_pop=True, popfile='qldppl.pop', dat
 if __name__ == '__main__':
     
     T = sc.tic()
-
     # Settings
     whattorun = ['calibration', 'scenarios'][1]
     domulti = True
-    doplot = False
+    doplot = True
     dosave = True
 
     # Filepaths
@@ -149,13 +197,16 @@ if __name__ == '__main__':
     # Run and plot
     if domulti:
         if whattorun == 'calibration':
-            sim = make_sim(whattorun, load_pop=True, popfile='qldppl.pop', datafile=datafile, agedatafile=agedatafile)
+            sim  = make_sim(whattorun, load_pop=True, popfile='qldppl.pop', datafile=datafile, agedatafile=agedatafile)
             msim = cv.MultiSim(base_sim=sim)
             msim.run(n_runs=20, reseed=True, noise=0)
             if dosave: msim.save(f'{resultsfolder}/qld_{whattorun}.obj')
             if doplot:
-                msim.plot(to_plot=to_plot, do_save=True, do_show=False, fig_path=f'qld_{whattorun}.png',
-                      legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=21)
+                msim.plot(to_plot=to_plot, do_save=True, do_show=False, 
+                          fig_path=f'qld_{whattorun}.png',
+                          legend_args={'loc': 'upper left'}, 
+                          axis_args={'hspace': 0.4}, 
+                          interval=21)
         elif whattorun == 'scenarios':
             julybetas = [0.1, 0.15, 0.2]
             for jb in julybetas:
@@ -164,8 +215,11 @@ if __name__ == '__main__':
                 msim.run(n_runs=20, reseed=True, noise=0)
                 if dosave: msim.save(f'{resultsfolder}/qld_{whattorun}_{int(jb*100)}.obj')
                 if doplot:
-                    msim.plot(to_plot=to_plot, do_save=True, do_show=False, fig_path=f'qld_{whattorun}_{int(jb*100)}.png',
-                          legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=21)
+                    msim.plot(to_plot=to_plot, do_save=True, do_show=False, 
+                              fig_path=f'qld_{whattorun}_{int(jb*100)}.png',
+                              legend_args={'loc': 'upper left'}, 
+                              axis_args={'hspace': 0.4}, 
+                              interval=21)
     else:
         sim = make_sim(whattorun, load_pop=True, popfile='qldppl.pop', datafile=datafile, agedatafile=agedatafile)
         sim.run()
