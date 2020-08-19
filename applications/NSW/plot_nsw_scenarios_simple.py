e2f273365990d85d4116ab8b9bac93f574eed7bc
Robyn Stuart <robyna.s@gmail.com>
2020-08-19 11:16:51 +1000

    nsw updates

--- applications/NSW/plot_nsw_scenarios_simple.py
+++ applications/NSW/plot_nsw_scenarios_simple.py
@@ -19,10 +19,9 @@ T = sc.tic()
 # Set up lists for storing results to be plotting
 diagprobs = []
 infprobs = []
-diagvals = []
-infvals = []
-layer_counts = []
-#colors = sc.gridcolors(n_new_layers)
+inf_med = []
+inf_low = []
+inf_high = []
 
 # Load objects
 for i,jb in enumerate(maskbetas):
@@ -33,26 +32,39 @@ for i,jb in enumerate(maskbetas):
     # Calculate probabilities
     diagprobs.append([len([i for i in range(100) if msim.sims[i].results['new_diagnoses'].values[-1]>j])/100 for j in range(500)])
     infprobs.append([len([i for i in range(100) if msim.sims[i].results['n_exposed'].values[-1]>j])/100 for j in range(6000)])
-    diagvals.append(msim.sims[0].results['new_diagnoses'].values[-60:])
-    infvals.append(msim.sims[0].results['n_exposed'].values[-60:])
-    msim.reduce()
-    if jb == 0.7: # Save these as the main scenario
-        sims = msim.sims
-        sim = sims[0]
 
-    # Now load the individual runs that contain the people for making transmission trees
-    tlc = sc.loadobj(f'results/nsw_layer_counts_{int(jb*100)}.obj')
-    layer_counts.append(tlc)
+    # Extract a sim for getting time vectors and so on
+    sims = msim.sims
+    sim = sims[0]
+
+    # Reduce sim and extract weekly values
+    msim.reduce()
+    w0, w1, w2, w3, w4 = cv.date('2020-08-16'), cv.date('2020-08-23'), cv.date('2020-08-30'), cv.date('2020-09-06'), cv.date('2020-09-13')
+    wd = [sim.day(w0), sim.day(w1), sim.day(w2), sim.day(w3), sim.day(w4)]
+    inf_med.append(msim.results['new_diagnoses'].values[wd])
+    inf_low.append(msim.results['new_diagnoses'].low[wd])
+    inf_high.append(msim.results['new_diagnoses'].high[wd])
+
+# Now load the transmission tree data
+tlc = sc.loadobj(f'results/nsw_layer_counts.obj')
+layer_counts = {}
+for jb in maskbetas: layer_counts[jb] = tlc[jb].sum(axis=0)/100 # Get mean
 
 # Sum transmission by layer over the relevant date ranges
 jun01 = cv.date('2020-06-01')
 aug15 = cv.date('2020-08-15')
 jun01d = sim.day(jun01)
 aug15d = sim.day(aug15)
-pre_counts = [layer_counts[j][jun01d:aug15d, :].sum(axis=0) for j in range(3)]
-post_counts = [layer_counts[j][aug15d:, :].sum(axis=0) for j in range(3)]
-pre_counts = [pre_counts[j]/pre_counts[j].sum()*100 for j in range(3)]
-post_counts = [post_counts[j]/post_counts[j].sum()*100 for j in range(3)]
+
+low_q = 0.1
+high_q = 0.9
+layer_counts_pre_med  = pl.median(tlc[0.7][:, jun01d:aug15d, :].sum(axis=1), axis=0)
+layer_counts_pre_low  = pl.quantile(tlc[jb][:, jun01d:aug15d, :].sum(axis=1), q=low_q, axis=0)
+layer_counts_pre_high = pl.quantile(tlc[jb][:, jun01d:aug15d, :].sum(axis=1), q=high_q, axis=0)
+for jb in maskbetas:
+    layer_counts_med  = [pl.median(tlc[jb][:, aug15d:, :].sum(axis=1), axis=0) for jb in maskbetas]
+    layer_counts_low  = [pl.quantile(tlc[jb][:, aug15d:, :].sum(axis=1), q=low_q, axis=0) for jb in maskbetas]
+    layer_counts_high = [pl.quantile(tlc[jb][:, aug15d:, :].sum(axis=1), q=high_q, axis=0) for jb in maskbetas]
 
 # Fonts and sizes
 font_size = 22
@@ -60,85 +72,97 @@ font_family = 'Proxima Nova'
 pl.rcParams['font.size'] = font_size
 pl.rcParams['font.family'] = font_family
 pl.figure(figsize=(24,15))
-pieargs = dict(startangle=90, counterclock=False, labeldistance=1.25)
 
-labels = ['Household', 'School', 'Workplace', 'Known community', 'Unknown community']
 ygaps = 0.06
-xgaps = 0.06
+xgaps = 0.065
 remainingy = 1-3*ygaps
 remainingx = 1-3*xgaps
 mainplotheight = remainingy/2
 mainplotwidth = remainingx/2
 
 # A. Probabilities of exceeding N daily diagnoses
+pl.figtext(xgaps*0.2, ygaps*2+mainplotheight*2, 'A', fontsize=40)
 x0, y0, dx, dy = xgaps, ygaps*2+mainplotheight, mainplotwidth, mainplotheight
 ax1 = pl.axes([x0, y0, dx, dy])
 colors = pl.cm.GnBu([0.9,0.6,0.3])
 ax1.plot(range(300), diagprobs[0][:300], '-', lw=4, c=colors[0], alpha=1.0)
 ax1.plot(range(300), diagprobs[1][:300], '-', lw=4, c=colors[1], alpha=1.0)
 ax1.plot(range(300), diagprobs[2][:300], '-', lw=4, c=colors[2], alpha=1.0)
-ax1.minorticks_on()
-pl.grid(which='minor', c=[0, 0, 0], linestyle='--', alpha=0.3, lw=1)
-pl.grid(which='major', c=[0, 0, 0], linestyle='--', alpha=0.3, lw=1)
+#ax1.minorticks_on()
+#pl.grid(which='minor', c=[0, 0, 0], linestyle='--', alpha=0.3, lw=1)
+#pl.grid(which='major', c=[0, 0, 0], linestyle='--', alpha=0.3, lw=1)
 ax1.set_ylim(0,1)
 pl.ylabel('Probability of more than n\ndaily cases within 4 weeks')
 sc.boxoff(ax=ax1)
 
 # B. Probabilities of exceeding N active cases
+pl.figtext(xgaps*1.2+mainplotwidth, ygaps*2+mainplotheight*2, 'B', fontsize=40)
 x0, y0, dx, dy = xgaps*2+mainplotwidth, ygaps*2+mainplotheight, mainplotwidth, mainplotheight
 ax2 = pl.axes([x0, y0, dx, dy])
-colors = pl.cm.hot([0.3,0.5,0.7])
+colors = pl.cm.GnBu([0.9,0.6,0.3]) #pl.cm.hot([0.3,0.5,0.7])
 ax2 = pl.axes([x0, y0, dx, dy])
-ax2.plot(range(6000), infprobs[0], '-', lw=4, c=colors[0], alpha=1.0)
-ax2.plot(range(6000), infprobs[1], '-', lw=4, c=colors[1], alpha=1.0)
-ax2.plot(range(6000), infprobs[2], '-', lw=4, c=colors[2], alpha=1.0)
+ax2.plot(range(6000), infprobs[0], '-', lw=4, c=colors[0], label="Victorian masks", alpha=1.0)
+ax2.plot(range(6000), infprobs[1], '-', lw=4, c=colors[1], label="Moderate masks", alpha=1.0)
+ax2.plot(range(6000), infprobs[2], '-', lw=4, c=colors[2], label="No masks", alpha=1.0)
 ax2.set_ylim(0,1)
-ax2.minorticks_on()
-pl.grid(which='minor', c=[0, 0, 0], linestyle='--', alpha=0.3, lw=1)
-pl.grid(which='major', c=[0, 0, 0], linestyle='--', alpha=0.3, lw=1)
+#ax2.minorticks_on()
+#pl.grid(which='minor', c=[0, 0, 0], linestyle='--', alpha=0.3, lw=1)
+#pl.grid(which='major', c=[0, 0, 0], linestyle='--', alpha=0.3, lw=1)
 pl.ylabel('Probability of more than n active\ninfections within 4 weeks')
+pl.legend(loc='upper right', frameon=False)
 sc.boxoff(ax=ax2)
 
-# Pie charts
+# C. bar chart
+pl.figtext(xgaps*0.2, ygaps*1+mainplotheight, 'C', fontsize=40)
 x0, y0, dx, dy = xgaps, ygaps, mainplotwidth, mainplotheight
-pie_ax1 = pl.axes([x0, y0, dx, dy])
+bar_ax = pl.axes([x0, y0, dx, dy])
+colors = sc.gridcolors(5)
+
+x = np.arange(5)
+colors = [(0.5,0.5,0.5), pl.cm.GnBu(0.3), pl.cm.GnBu(0.6), pl.cm.GnBu(0.9)]
+#layers_to_plot_med = [layer_counts_pre_med[0], layer_counts_med[2], layer_counts_med[1], layer_counts_med[0]]
+#layers_to_plot_low = [layer_counts_pre_low[0], layer_counts_low[2], layer_counts_low[1], layer_counts_low[0]]
+#layers_to_plot_high = [layer_counts_pre_high[0], layer_counts_high[2], layer_counts_high[1], layer_counts_high[0]]
+layers_to_plot_med = [layer_counts_med[2], layer_counts_med[1], layer_counts_med[0]]
+layers_to_plot_low = [layer_counts_low[2], layer_counts_low[1], layer_counts_low[0]]
+layers_to_plot_high = [layer_counts_high[2], layer_counts_high[1], layer_counts_high[0]]
+labels = ['Household', 'School', 'Workplace', 'Known\ncommunity', 'Unknown\ncommunity']
+#scenlabels = ['Jun 1 - Aug 15', 'Aug 16 - Sep 15: No masks', 'Aug 16 - Sep 15: Moderate masks', 'Aug 16 - Sep 15: Optimistic masks']
+scenlabels = ['No masks', 'Moderate masks', 'Victorian masks']
+
+for ltp in range(3):
+    bar_ax.bar(x+0.1*ltp-0.3, layers_to_plot_med[ltp], color=colors[ltp+1], width=0.1, label = scenlabels[ltp])
+    pl.plot([x+0.1*ltp-0.3,x+0.1*ltp-0.3], [layers_to_plot_low[ltp], layers_to_plot_high[ltp]], c='k')
+
+bar_ax.set_xticks(x-0.15)
+bar_ax.set_xticklabels(labels)
+#bar_ax.legend(frameon=False)
+pl.ylabel('Cumulative infections by layer\nAug 16 - Sep 15')
+sc.boxoff(ax=bar_ax)
+
+# D. box plot chart
+pl.figtext(xgaps*1.2+mainplotwidth, ygaps*1+mainplotheight, 'D', fontsize=40)
 x0, y0, dx, dy = xgaps*2+mainplotwidth, ygaps, mainplotwidth, mainplotheight
-pie_ax2 = pl.axes([x0, y0, dx, dy])
-colors = sc.gridcolors(n_new_layers)
+box_ax = pl.axes([x0, y0, dx, dy])
+x = np.arange(5)
+for ltp in range(3):
+    box_ax.errorbar(x+0.1*ltp-0.3, inf_med[ltp], yerr=[inf_low[ltp], inf_high[ltp]], fmt='o', color=colors[ltp+1], ecolor=colors[ltp+1], ms=20, elinewidth=3, capsize=0)
 
-lpre = [
-    f'Household\n{pre_counts[0][0]:0.1f}%',
-    f'School\n{pre_counts[0][1]:0.1f}% ',
-    f'Workplace\n{pre_counts[0][2]:0.1f}%    ',
-    f'Known community\n{pre_counts[0][3]:0.1f}%',
-    f'Unknown community\n{pre_counts[0][4]:0.1f}%',
-]
+box_ax.set_xticks(x-0.15)
+#box_ax.set_xticklabels(labels)
 
-lpost = [
-    f'Household\n{post_counts[0][0]:0.1f}%',
-    f'School\n{post_counts[0][1]:0.1f}%',
-    f'Workplace\n{post_counts[0][2]:0.1f}%',
-    f'Known community\n{post_counts[0][3]:0.1f}%',
-    f'Unknown community\n{post_counts[0][4]:0.1f}%',
-]
+@ticker.FuncFormatter
+def date_formatter(x, pos):
+    return (aug15 + dt.timedelta(days=x*7)).strftime('%b-%d')
 
-pie_ax1.pie(pre_counts[0], colors=colors, labels=lpre, **pieargs)
-pie_ax2.pie(post_counts[0], colors=colors, labels=lpost, **pieargs)
+box_ax.xaxis.set_major_formatter(date_formatter)
+pl.ylabel('Estimated daily infections')
+sc.boxoff(ax=box_ax)
 
-cv.savefig(f'{figsfolder}/nsw_scenarios_ts.png', dpi=100)
-
-'''
+#pl.xlim([0, sim['n_days']])
+#box_ax.legend(frameon=False)
 
 
-# Plot locations
-ygaps = 0.06
-xgaps = 0.06
-remainingy = 1-2*ygaps
-remainingx = 1-2*xgaps
-mainplotheight = remainingy
-mainplotwidth = remainingx/2
+cv.savefig(f'{figsfolder}/nsw_scenarios_new.png', dpi=100)
 
-
-cv.savefig(f'{figsfolder}/nsw_scenarios_simple.png', dpi=100)
-'''
 sc.toc(T)
--- applications/NSW/run_nsw.py
+++ applications/NSW/run_nsw.py
@@ -9,18 +9,18 @@ def make_sim(whattorun, mask_beta_change=None, load_pop=True, popfile='nswppl.po
     layers = ['H', 'S', 'W', 'C', 'church', 'pSport', 'cSport', 'entertainment', 'cafe_restaurant', 'pub_bar', 'transport', 'public_parks', 'large_events', 'social']
 
     if whattorun == 'calibration':
-        end_day = '2020-08-13'
+        end_day = '2020-09-20'
     elif whattorun == 'scenarios':
         end_day = '2020-09-15'
         mask_beta_change = mask_beta_change
 
     pars = {'pop_size': 100e3,
             'pop_infected': 110,
-            'pop_scale': 10,
+            'pop_scale': 75,
             'rescale': True,
             'rand_seed': 1,
-            'rel_death_prob': 0.8,
-            'beta': 0.02999, # Overall beta to use for calibration
+#            'rel_death_prob': 0.8,
+            'beta': 0.0253, # Overall beta to use for calibration
                                     # H     S       W       C       church  psport  csport  ent     cafe    pub     trans   park    event   soc
             'contacts':    pd.Series([4,    21,     5,      1,      20,     40,     30,     25,     19,     30,     25,     10,     50,     6], index=layers).to_dict(),
             'beta_layer':  pd.Series([1,    0.25,   0.3,    0.1,    0.04,   0.2,    0.1,    0.01,   0.04,   0.06,   0.16,   0.03,   0.01,   0.3], index=layers).to_dict(),
@@ -52,8 +52,8 @@ def make_sim(whattorun, mask_beta_change=None, load_pop=True, popfile='nswppl.po
                  cv.clip_edges(days=[lockdown, reopen2, reopen4], changes=[0, 0.5, 1.0], layers=['pSport'], do_plot=False),
                  cv.clip_edges(days=[lockdown, '2020-06-22'], changes=[0, 1.0], layers=['cSport'], do_plot=False),
 
-                 cv.change_beta([initresponse, '2020-07-01'], [0.8, 0.9], do_plot=False), # Reduce overall beta to account for distancing, handwashing, etc
-#                 cv.change_beta([initresponse], [0.8], do_plot=False), # Reduce overall beta to account for distancing, handwashing, etc
+#                 cv.change_beta([initresponse, '2020-07-01'], [0.8, 0.85], do_plot=False), # Reduce overall beta to account for distancing, handwashing, etc
+                 cv.change_beta([initresponse], [0.8], do_plot=False), # Reduce overall beta to account for distancing, handwashing, etc
                  cv.change_beta(days=[lockdown, reopen2, reopen4], changes=[1.2, 1.1, 1.], layers=['H'], do_plot=True),
 
                  cv.change_beta(days=[lockdown, reopen2], changes=[0, 0.7], layers=['church'], do_plot=False),
@@ -78,24 +78,32 @@ def make_sim(whattorun, mask_beta_change=None, load_pop=True, popfile='nswppl.po
     sim.pars['interventions'].extend(beta_ints)
 
     # Testing
-    symp_prob_prelockdown = 0.075  # Limited testing pre lockdown
+    symp_prob_prelockdown = 0.07  # Limited testing pre lockdown
     symp_prob_lockdown = 0.1  # Increased testing during lockdown
-    symp_prob_postlockdown = 0.2  # Testing since lockdown
+    symp_prob_postlockdown = 0.19  # Testing since lockdown
     sim.pars['interventions'].append(cv.test_prob(start_day=0, end_day=lockdown, symp_prob=symp_prob_prelockdown, asymp_quar_prob=0.01, do_plot=False))
-    sim.pars['interventions'].append(cv.test_prob(start_day=lockdown, end_day=reopen2, symp_prob=symp_prob_lockdown, asymp_quar_prob=0.04,do_plot=False))
-    sim.pars['interventions'].append(cv.test_prob(start_day=reopen2, symp_prob=symp_prob_postlockdown, asymp_quar_prob=0.05,do_plot=True))
+    sim.pars['interventions'].append(cv.test_prob(start_day=lockdown, end_day=reopen2, symp_prob=symp_prob_lockdown, asymp_quar_prob=0.01,do_plot=False))
+    sim.pars['interventions'].append(cv.test_prob(start_day=reopen2, symp_prob=symp_prob_postlockdown, asymp_quar_prob=0.02,do_plot=True))
 
     # Tracing
-    trace_probs = {'H': 1, 'S': 0.95, 'W': 0.8, 'C': 0.05, 'church': 0.5, 'pSport': 0.8, 'cSport': 0.5,
+    trace_probs_1 = {'H': 1, 'S': 0.95, 'W': 0.8, 'C': 0.05, 'church': 0.5, 'pSport': 0.8, 'cSport': 0.5,
                      'entertainment': 0.01, 'cafe_restaurant': 0.01, 'pub_bar': 0.01, 'transport': 0.01,
                      'public_parks': 0.01, 'large_events': 0.01, 'social': 0.9}
+    trace_probs_2 = {'H': 1, 'S': 0.95, 'W': 0.8, 'C': 0.01, 'church': 0.5, 'pSport': 0.8, 'cSport': 0.5,
+                 'entertainment': 0.25, 'cafe_restaurant': 0.25, 'pub_bar': 0.25, 'transport': 0.01,
+                 'public_parks': 0.01, 'large_events': 0.25, 'social': 0.9}
+#    trace_probs = {'H': 1, 'S': 0.95, 'W': 0.8, 'C': 0.3, 'church': 0.3, 'pSport': 0.8, 'cSport': 0.5,
+#                     'entertainment': 0.3, 'cafe_restaurant': 0.3, 'pub_bar': 0.3, 'transport': 0.01,
+#                     'public_parks': 0.01, 'large_events': 0.01, 'social': 0.8}
     trace_time = {'H': 0, 'S': 2, 'W': 2, 'C': 14, 'church': 5, 'pSport': 3, 'cSport': 3, 'entertainment': 14,
                     'cafe_restaurant': 14, 'pub_bar': 14, 'transport': 14, 'public_parks': 14, 'large_events': 14,
                     'social': 3}
-    sim.pars['interventions'].append(cv.contact_tracing(trace_probs=trace_probs, trace_time=trace_time, start_day=0, do_plot=False))
+#    sim.pars['interventions'].append(cv.contact_tracing(trace_probs=trace_probs_1, trace_time=trace_time, start_day=0, do_plot=False))
+    sim.pars['interventions'].append(cv.contact_tracing(trace_probs=trace_probs_1, trace_time=trace_time, start_day=0, end_day=reopen4, do_plot=False))
+    sim.pars['interventions'].append(cv.contact_tracing(trace_probs=trace_probs_2, trace_time=trace_time, start_day=123, do_plot=False))
 
     # Close borders, then open them again to account for Victorian imports and leaky quarantine
-    sim.pars['interventions'].append(cv.dynamic_pars({'n_imports': {'days': [14, 106, 115], 'vals': [0, 5, 0]}}, do_plot=False))
+    sim.pars['interventions'].append(cv.dynamic_pars({'n_imports': {'days': [14, 110, 115], 'vals': [0, 5, 0]}}, do_plot=False))
 
     sim.initialize()
 
@@ -108,10 +116,10 @@ def make_sim(whattorun, mask_beta_change=None, load_pop=True, popfile='nswppl.po
 T = sc.tic()
 
 # Settings
-whattorun = ['calibration', 'scenarios'][1]
-domulti = True
-doplot = False
-dosave = True
+whattorun = ['calibration', 'scenarios'][0]
+domulti = False
+doplot = True
+dosave = False
 
 # Filepaths
 inputsfolder = 'inputs'
@@ -184,7 +192,7 @@ else:
         if dosave: sim.save(f'{resultsfolder}/nsw_{whattorun}_single.obj',keep_people=True)
         if doplot:
             sim.plot(to_plot=to_plot, do_save=True, do_show=False, fig_path=f'nsw_{whattorun}.png',
-                  legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=21)
+                  legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=28)
     elif whattorun == 'scenarios':
         # Calculation of the mask_beta_change is (1-maskuptake)*0.7 + maskuptake*0.7*maskeff
         # Using values of maskuptake in [0, 0.5, 0.7] and maskeff in [0.05, 0.1, 0.2], this gives values in the range [0.6, 0.7]
@@ -195,6 +203,6 @@ else:
             if dosave: sim.save(f'{resultsfolder}/nsw_{whattorun}_{int(jb*100)}_single.obj',keep_people=True)
             if doplot:
                 sim.plot(to_plot=to_plot, do_save=True, do_show=False, fig_path=f'nsw_{whattorun}_{int(jb*100)}.png',
-                      legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=21)
+                      legend_args={'loc': 'upper left'}, axis_args={'hspace': 0.4}, interval=28)
 
 sc.toc(T)
