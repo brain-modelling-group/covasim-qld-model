ec74db4184ac41f6a28acf9edbef660fe9a3da15
Romesh Abeysuriya <romesh.abey@gmail.com>
2020-09-01 15:58:11 +0800

    Update parameters to match actual runs

--- applications/Susceptibility/outbreak/main.py
+++ applications/Susceptibility/outbreak/main.py
@@ -67,7 +67,7 @@ def load_australian_parameters(location: str = 'Victoria', pop_size: int = 1e4, 
     epi_name = 'epi_data_Australia'
 
     all_lkeys = ['H', 'S', 'W', 'C', 'church', 'cSport', 'entertainment', 'cafe_restaurant', 'pub_bar',
-                 'transport', 'public_parks', 'large_events', 'child_care', 'social']
+                 'transport', 'public_parks', 'large_events', 'child_care', 'social','aged_care']
     dynamic_lkeys = ['C', 'entertainment', 'cafe_restaurant', 'pub_bar',
                      'transport', 'public_parks', 'large_events']
 
@@ -75,13 +75,13 @@ def load_australian_parameters(location: str = 'Victoria', pop_size: int = 1e4, 
                             'pop_infected': 0,
                             'pop_scale': 1,
                             'rescale': 0,
-                            'beta': 0.057,
+                            'beta': 0.065,
                             'n_days': n_days,
                             'calibration_end': None,
-                            'verbose': 1}}
+                            'verbose': 0}}
 
     metapars = {'noise': 0.0,
-                'verbose': 1}
+                'verbose': 0}
 
     user_pars, calibration_end = utils.clean_pars(user_pars, [location])
 
@@ -194,7 +194,7 @@ def get_australia_outbreak(seed: int, params: parameters.Parameters, scen_polici
                                                                    dynamic_layers=params.dynamic_lkeys))
 
     sim.pars['interventions'].append(policy_updates.AppBasedTracing(name='tracing_app',
-                                                                    layers=['H', 'S', 'W', 'C', 'church', 'cSport', 'entertainment', 'cafe_restaurant', 'pub_bar', 'transport', 'public_parks', 'large_events', 'child_care', 'social'],
+                                                                    layers=['H', 'S', 'W', 'C', 'church', 'cSport', 'entertainment', 'cafe_restaurant', 'pub_bar', 'transport', 'public_parks', 'large_events', 'child_care', 'social', 'aged_care'],
                                                                     coverage=[0.2],
                                                                     days=[0]))
 
