5198dda1c44f4eef7b2f0898b03f696ee7c90906
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-09-28 23:11:47 +1000

    Write script to launch multiple simulations

---
+++ applications/qld-aus/run_calibration_moving_end_dates.sh
@@ -0,0 +1,15 @@
+#/usr/bin/bash
+# Run calibration of QLD covid model up to END_DATE
+
+# Paula Sanz-Leon, QIMR Berghofer September 2020
+
+
+START_DATE="$(date -d "2020-08-05" +%s)"
+END_DATE="$(date -d "2020-09-15" +%s)"
+
+while [[ "$START_DATE" -le "$END_DATE" ]]; do 
+    THIS_DATE="$(date -d "@$START_DATE" +%F)"
+    echo $THIS_DATE
+    python run_qld_wave_02.py --nruns 20 --case calibration --end_calibration_date "$THIS_DATE" 
+    let START_DATE+=86400;
+done
