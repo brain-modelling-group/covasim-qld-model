6df3d464e8483cfbc1b653970ac958333cde3980
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2020-10-01 19:07:06 +1000

    Save prediction settings

--- applications/qld-aus/run_scenarios_borders_infection_rates.sh
+++ applications/qld-aus/run_scenarios_borders_infection_rates.sh
@@ -2,8 +2,8 @@
 # Run calibration of QLD covid model up to END_DATE
 
 # Paula Sanz-Leon, QIMR Berghofer September 2020
-for x in {0..15}; do
+for x in {0..50}; do
     THIS_NUMBER=`bc <<< "scale=1; $x/10"`
     #echo $THIS_NUMBER
-    python run_qld_wave_02.py --nruns 100 --case scenarios --par1 $THIS_NUMBER --end_simulation_date '2020-12-31' --open_borders_date '2020-10-09'
+    python run_qld_wave_02.py --nruns 100 --case scenarios --par1 $THIS_NUMBER --end_simulation_date '2020-12-31' --open_borders_date '2020-10-01'
 done
