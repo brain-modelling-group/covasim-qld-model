2c31e13c0f8b0e73183efccbd185b689122fb4b2
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-01-18 12:38:59 +1000

    Adjust end date and ranges

--- applications/qld-aus/launch_recalibration_2D_pse.sh
+++ applications/qld-aus/launch_recalibration_2D_pse.sh
@@ -3,11 +3,11 @@
 # 
 
 # Paula Sanz-Leon, QIMR Berghofer September 2020
-for x in {1..20}; do
+for x in {1..25}; do
     THIS_NUMBER=$x
-    for b in $(seq 0.01 0.0005 0.04); do
+    for b in $(seq 0.01 0.0005 0.03); do
     	BETA=$b
         #echo $THIS_NUMBER
-        python run_qld_model.py --nruns 7 --init_seed_infections $THIS_NUMBER --global_beta $BETA --end_calibration_date '2020-05-15'
+        python run_qld_model.py --nruns 7 --init_seed_infections $THIS_NUMBER --global_beta $BETA --end_calibration_date '2020-03-30'
     done
 done
