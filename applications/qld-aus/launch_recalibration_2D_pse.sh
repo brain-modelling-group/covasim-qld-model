846154c7530b9560ceb545821a6eaa89af0a08a3
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-01-18 10:41:56 +1000

    Write 2D pse launcher for workstation

--- applications/qld-aus/launch_recalibration_2D_pse.sh
+++ applications/qld-aus/launch_recalibration_2D_pse.sh
@@ -5,6 +5,9 @@
 # Paula Sanz-Leon, QIMR Berghofer September 2020
 for x in {1..20}; do
     THIS_NUMBER=$x
-    #echo $THIS_NUMBER
-    python run_qld_model.py --nruns 7 --init_seed_infections $THIS_NUMBER --end_calibration_date '2020-05-15'
+    for b in $(seq 0.01 0.0005 0.04); do
+    	BETA=$b
+        #echo $THIS_NUMBER
+        python run_qld_model.py --nruns 7 --init_seed_infections $THIS_NUMBER --global_beta $BETA --end_calibration_date '2020-05-15'
+    done
 done
