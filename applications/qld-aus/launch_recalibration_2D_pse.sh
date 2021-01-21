11fe99804af6a47df18e1b8ccc7fc95220313ec2
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-01-21 16:19:39 +1000

    Run 15 sims per combination of parameters

--- applications/qld-aus/launch_recalibration_2D_pse.sh
+++ applications/qld-aus/launch_recalibration_2D_pse.sh
@@ -8,6 +8,6 @@ for x in {1..25}; do
     for b in $(seq 0.01 0.0005 0.03); do
     	BETA=$b
         #echo $THIS_NUMBER
-        python run_qld_model_refined_betas.py --nruns 5 --init_seed_infections $THIS_NUMBER --global_beta $BETA --end_calibration_date '2020-05-31'
+        python run_qld_model_refined_betas.py --nruns 15 --init_seed_infections $THIS_NUMBER --global_beta $BETA --end_calibration_date '2020-05-31'
     done
 done
