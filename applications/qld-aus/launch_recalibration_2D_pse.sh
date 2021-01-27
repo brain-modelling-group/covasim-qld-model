db388ce38ff2be56899bfe638e8c16a79487e37a
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-01-27 15:33:19 +1000

    Include second case in launcher

--- applications/qld-aus/launch_recalibration_2D_pse.sh
+++ applications/qld-aus/launch_recalibration_2D_pse.sh
@@ -3,11 +3,21 @@
 # 
 
 # Paula Sanz-Leon, QIMR Berghofer September 2020
-for x in {1..25}; do
+for x in {1..50}; do
     THIS_NUMBER=$x
     for b in $(seq 0.01 0.0005 0.03); do
-    	BETA=$b
+        BETA=$b
         #echo $THIS_NUMBER
-        python run_qld_model_refined_betas.py --nruns 2 --init_seed_infections $THIS_NUMBER --global_beta $BETA --start_calibration_date '2020-02-15' --end_calibration_date '2020-05-15'
+        python run_qld_model_refined_betas.py --nruns 10 --init_seed_infections $THIS_NUMBER --global_beta $BETA --start_calibration_date '2020-02-15' --end_calibration_date '2020-05-15'
+    done
+done
+
+# Do the same but for new tests that have been convolved
+for x in {1..50}; do
+    THIS_NUMBER=$x
+    for b in $(seq 0.01 0.0005 0.03); do
+        BETA=$b
+        #echo $THIS_NUMBER
+        python run_qld_model_refined_betas.py --nruns 10 --init_seed_infections $THIS_NUMBER --global_beta $BETA --start_calibration_date '2020-02-15' --end_calibration_date '2020-05-15' --new_tests_mode 'mav'
     done
 done
