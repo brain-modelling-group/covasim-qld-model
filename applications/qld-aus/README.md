c7651f4dfde969c86b1069a89224dc479ec1ab76
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-01-28 11:07:26 +1000

    Clean up repo

--- applications/qld-aus/README.md
+++
@@ -1,5 +0,0 @@
-# Modelling COVID-19 in  Queensland Australia with covasim
-
-The main code is in `covasim-australia/applications/qld-aus/`
-
-## Usage
--- applications/qld-aus/run_scenarios_borders_infection_rates.sh
+++
@@ -1,9 +0,0 @@
-#/usr/bin/bash
-# Run calibration of QLD covid model up to END_DATE
-
-# Paula Sanz-Leon, QIMR Berghofer September 2020
-for x in {50..10}; do
-    THIS_NUMBER=`bc <<< "scale=1; $x/1"`
-    #echo $THIS_NUMBER
-    python run_qld_wave_02.py --nruns 10 --case scenarios --par1 $THIS_NUMBER --end_simulation_date '2021-12-31'
-done
