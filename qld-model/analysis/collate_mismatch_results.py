311ab9c750a4f296eccfed5ed7d6761f8ceb8e9a
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-02-02 12:42:40 +1000

    Use appropriate string formatting syntax

--- qld-model/analysis/collate_mismatch_results.py
+++ qld-model/analysis/collate_mismatch_results.py
@@ -126,8 +126,8 @@ if __name__ == '__main__':
     fit_results_stored_as = 'list' 
 
     if fit_results_stored_as == 'list':
-        output_dict = collate_mismatch_results_list(betas, seed_infections, file_string, results_path+results_folder)
+        output_dict = collate_mismatch_results_list(betas, seed_infections, file_string, f'{results_path}{results_folder}')
     else:
-        output_dict = collate_mismatch_results_dict(betas, seed_infections, file_string, results_path+results_folder)
+        output_dict = collate_mismatch_results_dict(betas, seed_infections, file_string, f'{results_path}{results_folder}')
 
     save_mismatch_results(results_path, output_dict, file_string)
