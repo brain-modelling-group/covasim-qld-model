d6c344f12ffcb93700bd34a94ce3efcec0f72fd6
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-02-15 17:41:01 +1000

    Write local launcher for testing probabilities

--- qld-model/launchers/bash/launch_recalibration_testing_probs.sh
+++ qld-model/launchers/bash/launch_recalibration_testing_probs.sh
@@ -3,13 +3,13 @@
 # 
 
 # Paula Sanz-Leon, QIMR Berghofer September 2020
-for par1 in $(seq 1.0 0.002 1.1);; do
-    P1=$par1
-    for par2 in $(seq 1.0 0.025 1.5); do
-        P2=$par2
-        python run_qld_model_refined_betas.py --ncpus 10 --nruns 10 --init_seed_infections 99 --global_beta 0.0105 \
-                                      --p1 "$P1" --p2 "$P2" \
-                                      --start_calibration_date '2020-02-15' --end_simulation_date '2020-04-10' --end_calibration_date '2020-03-30'\
+for par2 in $(seq 0.6 0.2 1.4); do
+    P2=$par2
+    for par1 in $(seq 0.0 0.05 0.3); do
+        P1=$par1
+        python run_qld_model_refined_betas.py --ncpus 10 --nruns 5 --init_seed_infections 359 --global_beta 0.0105 \
+                                      --par1 "$P1" --par2 "$P2" \
+                                      --start_calibration_date '2020-03-01' --end_simulation_date '2020-04-25' --end_calibration_date '2020-03-30'\
                                       --layer_betas_file 'qld_model_layer_betas_01.csv' \
                                       --epi_calibration_file 'qld_epi_data_qld-health_calibration_2020-02-15_2020-03-30.csv'\
                                       --new_tests_mode 'mav'
