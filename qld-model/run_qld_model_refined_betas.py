6e16164e7462d3bc5ad9416221a8f0633c20d4af
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-02-15 14:25:55 +1000

    Introduce ability to change sympt testing probabilities, asymp quarantine probabilities

--- qld-model/run_qld_model_refined_betas.py
+++ qld-model/run_qld_model_refined_betas.py
@@ -159,7 +159,8 @@ def make_sim(load_pop=True, popfile='qldppl.pop', datafile=None, agedatafile=Non
     beta_ints = define_beta_changes(betasfile, layers)             
     sim.pars['interventions'].extend(beta_ints)
 
-    # Testing
+    # Testing interventions
+    # Testing numbers
     data = pd.read_csv(datafile, parse_dates=['date'])
     if input_args.new_tests_mode == 'raw':
        this_column = 'new_tests_raw'
@@ -170,6 +171,41 @@ def make_sim(load_pop=True, popfile='qldppl.pop', datafile=None, agedatafile=Non
 
     sim.pars['interventions'].append(cv.test_num(daily_tests=new_tests))
 
+    # Testing probabilties of symptomatic -- from NSW cases
+    symp_test_prob_prelockdown = 0.04  # Limited testing pre lockdown
+    symp_test_prob_lockdown = 0.07     # 0.065 #Increased testing during lockdown
+    
+    
+    lockdown_date = '2020-03-30'  # Lockdown date in QLD
+    reopen_date   = '2020-05-15'  # Reopen shops etc date in QLD-NSW
+    sim.pars['interventions'].append([cv.test_prob(start_day=input_args.start_calibration_date, 
+                                                   end_day=lockdown_date, 
+                                                   symp_prob=symp_test_prob_prelockdown, 
+                                                   asymp_quar_prob=0.01, do_plot=False),
+                                       cv.test_prob(start_day=lockdown_date, 
+                                                    end_day=reopen_date, 
+                                                    symp_prob=symp_test_prob_lockdown, 
+                                                    asymp_quar_prob=0.01,do_plot=False)])
+
+    if sim.day(input_args.end_simulation_date) > sim.day(reopen_date):     
+        # More assumptions from NSW
+        symp_test_prob_postlockdown = 0.19 # 0.165 # Testing since lockdown
+        asymp_quar_prob_postlockdown = (1.0-(1.0-symp_prob_postlockdown)**10)
+        
+        reopen2_date = '2020-12-01' # Start of stage 6 in QLD
+        sim.pars['interventions'].append(cv.test_prob(start_day=reopen_date, 
+                                                      end_day=reopen2_date, 
+                                                      symp_prob=symp_test_prob_postlockdown, 
+                                                      asymp_quar_prob=asymp_quar_prob_postlockdown,do_plot=True))
+
+    if sim.day(input_args.end_simulation_date) > sim.day(reopen2_date):
+        # Future interventions, from start of stage 6 onwards
+        symp_test_prob_future = 0.9 # From NSW cases
+        asymp_quar_prob_future = (1.0-(1.0-future_test_prob)**10)/2.0 
+
+        sim.pars['interventions'].append(cv.test_prob(start_day=reopen2_date, 
+                                                      symp_prob=symp_test_prob_future, 
+                                                      asymp_quar_prob=asymp_quar_prob_future,do_plot=True))
 
     # Tracing
     trace_probs = {'H': 1.00, 'S': 0.95, 
