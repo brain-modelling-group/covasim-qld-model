5c8e9d42ab5c2fd6bf00a3a077555f2ed383fefc
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-02-16 21:21:32 +1000

    Get rid of unused analyser

--- qld-model/run_qld_model_refined_betas.py
+++ qld-model/run_qld_model_refined_betas.py
@@ -156,7 +156,6 @@ def make_sim(load_pop=True, popfile='qldppl.pop', datafile=None, agedatafile=Non
             'n_imports': 0.0, # Number of new cases per day -- can be varied over time as part of the interventions
             'start_day': input_args.start_calibration_date,
             'end_day':   input_args.end_simulation_date,
-            'analyzers': cv.age_histogram(datafile=agedatafile, edges=np.linspace(0, 75, 16), days=[8, 54]), # These days correspond to dates 9 March and 24 April, which is the date window in which qld has published age-disaggregated case counts
             'verbose': 0}
 
     sim = cv.Sim(pars=pars,
