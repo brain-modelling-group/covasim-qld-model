aa56252309becbaf3101a22a1a3f6515ac8f8920
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-02-17 18:28:45 +1000

    Update input data filename

--- qld-model/analysis/calculate_new_tests_moving_average.py
+++ qld-model/analysis/calculate_new_tests_moving_average.py
@@ -7,22 +7,22 @@ Calculate moving average for new tests
 
 import numpy as np
 import pandas as pd
-import covasim.misc as cvm
 
 
 # Load empirical data
 inputs_folder = 'inputs'
-input_data = 'qld_health_epi_data_calibration.csv'
+input_data = 'qld_epi_data_qld-health.csv'
+output_data = 'qld_epi_data_qld-health_mav9.csv'
 
 # Load data
 data = pd.read_csv("/".join((inputs_folder, input_data)), parse_dates=['date'])
 
 xx = data['new_tests_raw'].astype(float)
 
-num_days = 15
+num_days = 9
 xx = np.convolve(xx, np.ones((num_days, ))/num_days, mode='same')
 
 data['new_tests'] = np.round(xx)
 
 
-data.to_csv("/".join((inputs_folder, input_data)))
+data.to_csv("/".join((inputs_folder, output_data)))
