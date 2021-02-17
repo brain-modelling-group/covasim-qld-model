35e10063246b2e73725bc9828c4c6a69fdd7a3b8
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-02-17 18:28:19 +1000

    Update function call

--- qld-model/launchers/bash/launch_recalibration_testing_probs.sh
+++ qld-model/launchers/bash/launch_recalibration_testing_probs.sh
@@ -2,12 +2,12 @@
 # Run calibration of QLD covid model with different number of initial infections
 # 
 # Paula Sanz-Leon, QIMR Berghofer Feb 2021
-for par1 in $(seq 0.5 0.5 2.5); do
+for par1 in $(seq 0.05 0.05 0.1); do
     P1=$par1
-        python run_qld_model_optimise_testing.py --ncpus 10 --nruns 5 --init_seed_infections 99 --global_beta 0.0105 \
+        python run_qld_model_optimise_testing.py --ncpus 10 --nruns 5 --init_seed_infections 150 --global_beta 0.014 \
                                       --p1 "$P1" \
-                                      --start_calibration_date '2020-02-15' --end_simulation_date '2020-05-15' --end_calibration_date '2020-05-15'\
+                                      --start_calibration_date '2020-03-01' --end_simulation_date '2020-04-05' --end_calibration_date '2020-04-05'\
                                       --layer_betas_file 'qld_model_layer_betas_01.csv' \
-                                      --epi_calibration_file 'qld_epi_data_qld-health_calibration_2020-02-15_2020-05-15.csv'\
-                                      --new_tests_mode 'raw'
+                                      --epi_calibration_file 'qld_epi_data_qld-health_calibration_2020-02-15_2020-05-15_mav05.csv'\
+                                      --new_tests_mode 'mav05'
 done
