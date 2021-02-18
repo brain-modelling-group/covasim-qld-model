a8d6461ad77b73e6964a99b6d1c54f7ad7199664
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-02-18 18:16:32 +1000

    Run 25 trials for a quick sweep

--- qld-model/run_qld_model_optimise_testing_nums.py
+++ qld-model/run_qld_model_optimise_testing_nums.py
@@ -301,7 +301,7 @@ if __name__ == '__main__':
     
      # Settings
     n_workers = 1 # Define how many workers to run in parallel
-    n_trials = 100 # Define the number of trials, i.e. sim runs, per worker
+    n_trials = 25 # Define the number of trials, i.e. sim runs, per worker
     name      = 'my-example-calibration'
     db_name   = f'{name}.db'
     storage   = f'sqlite:///{db_name}'
