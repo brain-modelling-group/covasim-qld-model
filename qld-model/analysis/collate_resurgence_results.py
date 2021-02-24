b024bea82beff7a593e6144124511e3dd825d8e8
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-02-24 11:16:27 +1000

    Fix path problems

--- qld-model/analysis/collate_resurgence_results.py
+++ qld-model/analysis/collate_resurgence_results.py
@@ -15,11 +15,16 @@ import argparse
 parser = argparse.ArgumentParser()
 
 
-parser.add_argument('--filename', 
-                        type=str, 
-                        help='''The relative and/or absolute path to a file with csv file names where results are stored''')
 
-parser.add_argument('--resultname', 
+parser.add_argument('--filelist_name', 
+                        type=str, 
+                        help='''The name of  ile with csv file names where results are stored''')
+
+parser.add_argument('--filelist_path', 
+                        type=str, 
+                        help='''The absolute path to a file with csv file names where results are stored''')
+
+parser.add_argument('--result_name', 
                         type=str, 
                         help='''The relative and/or absolute path to a results csv file''')
 args = parser.parse_args()
@@ -33,6 +38,6 @@ def load_file(fname):
 with open(args.filename, 'r') as f:
         content = f.readlines()
         filelist = [l.strip() for l in content] 
-        frames = [load_file(fname) for fname in filelist]
+        frames = [load_file(f"{args.filelist_path}/{fname}") for fname in filelist]
         result = pd.concat(frames)
-        result.to_csv(args.resultname)
+        result.to_csv(f"{args.filelist_path}/{args.resultname}")
