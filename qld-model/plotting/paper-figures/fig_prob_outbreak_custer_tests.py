b3708001ed0716fc09c07857328d8ab3c6b1c47a
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-03-16 11:43:38 +1000

    Update

--- qld-model/plotting/paper-figures/fig_prob_outbreak_custer_tests.py
+++ qld-model/plotting/paper-figures/fig_prob_outbreak_custer_tests.py
@@ -10,13 +10,15 @@ import covasim as cv
 sns.set_context("poster")
 
 # Import data 
-df_cluk = pd.read_csv('/home/paula/data_ext4/Dropbox/COVID/simulated-data/resurgence/outbreak_cluster_size_uk.csv')
+#df_cluk = pd.read_csv('/home/paula/data_ext4/Dropbox/COVID/simulated-data/resurgence/outbreak_cluster_size_uk.csv')
+df_cluk = pd.read_csv('/home/paula/Dropbox/COVID/simulated-data/resurgence/outbreak_cluster_size_uk.csv')
+
 
 def get_subframe(df, num_tests, iq_factor):
     return df[(df["num_tests"] == num_tests) & (df["iq_factor"] == iq_factor)]
 
 # Select one lvel of testing and one level of iq
-iq_factor = 0.1
+iq_factor = 1.0
 
 fig, ax1 = plt.subplots(figsize=(9,7))
 color = 'tab:blue'
@@ -24,7 +26,7 @@ ax1.set_xlabel('cluster size')
 ax1.set_ylabel('prob outbreak [%]')
 ax1.set_xlim([0, 17])
 
-category_colors = plt.get_cmap('viridis')(np.linspace(0.25, 0.75, 5))
+category_colors = plt.get_cmap('inferno')(np.linspace(0.25, 0.75, 5))
 
 ls1 = []
 for idx, nt in enumerate([4160, 6260, 8360, 10460, 12560]):
@@ -36,7 +38,7 @@ for idx, nt in enumerate([4160, 6260, 8360, 10460, 12560]):
 labs = [l.get_label() for l in ls1]
 ax1.legend(ls1, labs, loc=0, frameon=False)
 fig.tight_layout()
-cv.savefig(f"fig_prob_outbreak_cluster_tests.png", dpi=300)
+cv.savefig(f"fig_prob_outbreak_cluster_tests_iq_1.0.png", dpi=300)
 
 plt.show()
 
