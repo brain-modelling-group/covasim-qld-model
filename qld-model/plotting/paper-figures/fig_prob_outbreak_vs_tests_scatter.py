7ea439cba8ac89e6c5ec77b8216689d6820551df
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-05-06 08:57:44 +1000

    Save

--- qld-model/plotting/paper-figures/fig_prob_outbreak_vs_tests_scatter.py
+++ qld-model/plotting/paper-figures/fig_prob_outbreak_vs_tests_scatter.py
@@ -7,7 +7,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import covasim as cv
 
-sns.set_context("poster")
+sns.set_context("paper")
 
 # Import data 
 df_cluk = pd.read_csv('/home/paula/data_ext4/Dropbox/COVID/simulated-data/resurgence/outbreak_cluster_size_uk.csv')
@@ -18,27 +18,27 @@ def get_subframe(df, num_tests, iq_factor):
     return df[(df["num_tests"] == num_tests) & (df["iq_factor"] == iq_factor)]
 
 # Select one lvel of testing and one level of iq
-iq_factor = 1.0
+iq_factor = 0.1
 
-fig, ax1 = plt.subplots(figsize=(9,7))
+fig, ax1 = plt.subplots(figsize=(6,3.6))
 color = 'tab:blue'
 ax1.set_xlabel('cluster size')
-ax1.set_ylabel('prob outbreak [%]')
-ax1.set_xlim([0, 17])
+ax1.set_ylabel('P[outbreak detection] [%]')
+ax1.set_xlim([1, 10])
 
-category_colors = plt.get_cmap('inferno')(np.linspace(0.25, 0.75, 5))
+category_colors = plt.get_cmap('coolwarm')(np.linspace(0.25, 0.75, 3))
 
 ls1 = []
-for idx, nt in enumerate([4160, 6260, 8360, 10460, 12560]):
+for idx, nt in enumerate([4160, 6260, 12560]):
     data = get_subframe(df_cluk, nt, iq_factor)
-    ls1.append(ax1.scatter(data["cluster_size"], data["outbreak_prob"], color=category_colors[idx, ...], label=f"{nt}"))
+    ls1.append(ax1.plot(data["cluster_size"], data["outbreak_prob"], color=category_colors[idx, ...]*0.8, lw=2, label=str(nt)))
 
 
 # Labels for legend
-labs = [l.get_label() for l in ls1]
-ax1.legend(ls1, labs, loc=0, frameon=False)
+handler1, label1 = ax1.get_legend_handles_labels()
+ax1.legend(handler1, label1, loc="lower right", frameon=True, title='number of daily tests')
 fig.tight_layout()
-#cv.savefig(f"fig_prob_outbreak_cluster_tests_iq_1.0.png", dpi=300)
+cv.savefig(f"fig_prob_outbreak_cluster_tests_iq_0.1.png", dpi=300)
 
 plt.show()
 
