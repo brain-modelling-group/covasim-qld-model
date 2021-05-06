55c68bb38238c17b14fc7f59f0dd45f49cb5e92d
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-05-07 00:27:57 +1000

    Save

--- qld-model/plotting/paper-figures/fig_prob_outbreak_map_vs_test.py
+++ qld-model/plotting/paper-figures/fig_prob_outbreak_map_vs_test.py
@@ -17,9 +17,9 @@ df_pluk = pd.read_csv('/home/paula/data_ext4/Dropbox/COVID/simulated-data/resurg
 
 # Select one lvel of testing and one level of iq
 num_tests = 8360
-iq_factor = 0.1
-cluster_size_th = 10
-cluster_col = "cluster_size"
+iq_factor = 1.0
+cluster_size_th = 1.0
+cluster_col = "iq_factor"
 poisson_th = 1.0
 poisson_col = "poisson_lambda"
 
@@ -28,7 +28,8 @@ def get_subframe(df, num_tests, par_column_name, col_th, day_column_name):
     if par_column_name == "poisson_lambda":
         df_sub = df_sub[(df_sub[par_column_name] <=col_th) & (df_sub[par_column_name] >=0.5) & (df_sub["iq_factor"] == iq_factor)]
     else:
-        df_sub = df_sub[(df_sub[par_column_name] <=col_th) & (df_sub["iq_factor"] == iq_factor)]
+        #df_sub = df_sub[(df_sub[par_column_name] <=col_th) & (df_sub["iq_factor"] == iq_factor)]
+        df_sub = df_sub[(df_sub[par_column_name] <=col_th) & (df_sub["cluster_size"] == 20)]
 
     df_sub = df_sub[["num_tests", day_column_name, par_column_name]]
     df_map = df_sub.pivot("num_tests", par_column_name, day_column_name)
@@ -38,11 +39,11 @@ def plot_heatmaps(df_map_list, fig_name_list, xlab, cmap_name='inferno'):
 
     for df_map, fig_name in zip(df_map_list, fig_name_list):
         f, ax = plt.subplots(figsize=(14, 6))
-        sns.heatmap(df_map, annot=True, fmt=".0f", linewidths=.5, ax=ax, cmap=cmap_name, vmin=0, vmax=100)
+        sns.heatmap(df_map, annot=True, fmt=".0f", linewidths=.5, ax=ax, cmap=cmap_name, vmin=0, vmax=40, cbar_kws={'label': 'days'})
         ax.set_ylabel('daily number of tests')
         ax.set_xlabel(xlab)
         f.tight_layout()
-        cv.savefig(fig_name, dpi=300)
+        #cv.savefig(fig_name, dpi=300)
     return
 
 # dfcloz_fc_map = get_subframe(df_cloz, num_tests, cluster_col, cluster_size_th, "outbreak_prob") 
@@ -54,12 +55,12 @@ def plot_heatmaps(df_map_list, fig_name_list, xlab, cmap_name='inferno'):
 # plot_heatmaps([dfploz_fc_map, dfpluk_fc_map], [f"fig_outbreak_vs_tests_poisson_oz.png", "fig_outbreak_vs_tests_poisson_uk.png"], 'daily arrivals')
 
 
-dfcloz_fc_map = get_subframe(df_cloz, num_tests, cluster_col, cluster_size_th, "outbreak_inf_prob") 
-dfcluk_fc_map = get_subframe(df_cluk, num_tests, cluster_col, cluster_size_th, "outbreak_inf_prob") 
-dfploz_fc_map = get_subframe(df_ploz, num_tests, poisson_col, poisson_th, "outbreak_inf_prob") 
-dfpluk_fc_map = get_subframe(df_pluk, num_tests, poisson_col, poisson_th, "outbreak_inf_prob") 
+dfcloz_fc_map = get_subframe(df_cloz, num_tests, cluster_col, cluster_size_th, "outbreak_day_av") 
+dfcluk_fc_map = get_subframe(df_cluk, num_tests, cluster_col, cluster_size_th, "outbreak_day_av") 
+#dfploz_fc_map = get_subframe(df_ploz, num_tests, poisson_col, poisson_th, "outbreak_inf_prob") 
+#dfpluk_fc_map = get_subframe(df_pluk, num_tests, poisson_col, poisson_th, "outbreak_inf_prob") 
 
-plot_heatmaps([dfcloz_fc_map, dfcluk_fc_map], [f"fig_resurgence_vs_tests_cluster_oz.png", "fig_resurgence_vs_tests_cluster_uk.png"], 'cluster size', cmap_name="viridis")
-plot_heatmaps([dfploz_fc_map, dfpluk_fc_map], [f"fig_resurgence_vs_tests_poisson_oz.png", "fig_resurgence_vs_tests_poisson_uk.png"], 'daily arrivals', cmap_name="viridis")
+plot_heatmaps([dfcloz_fc_map, dfcluk_fc_map], [f"fig_resurgence_vs_tests_cluster_oz.png", "fig_resurgence_vs_tests_cluster_uk.png"], 'Q/I leakage', cmap_name="inferno_r")
+#plot_heatmaps([dfploz_fc_map, dfpluk_fc_map], [f"fig_resurgence_vs_tests_poisson_oz.png", "fig_resurgence_vs_tests_poisson_uk.png"], 'daily arrivals', cmap_name="viridis")
 
 plt.show()
