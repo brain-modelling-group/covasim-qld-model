1abc2de5cf74e1e3bb3ea01b517266680d5891e4
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-05-26 16:14:41 +1000

    Generate sup figs 03 and 05

---
+++ qld-model/plotting/paper-figures/supfig03_ab_poisson_timeseries.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+# coding: utf-8
+"""
+Plot QLD results obtained with run_qld_wave_02.py with 
+different calibration end dates 
+=============================================================
+
+
+# author: For QLD Paula Sanz-Leon, QIMRB, September 2020
+"""
+
+import pandas as pd
+import sciris as sc
+import pylab as pl
+import numpy as np
+from matplotlib import ticker
+import datetime as dt
+import matplotlib.patches as patches
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+sns.set_context("paper", font_scale=1.9)
+
+# Filepaths
+results_folder = '/home/paula/Dropbox/COVID/simulated-data/resurgence/case-ploz'
+#results_folder = '/home/paula/Dropbox/COVID/simulated-data/resurgence/case-pluk'
+
+figure_folder = '/home/paula/Work/Articles/coronavirus-qld-calibration/figures'
+figure_filename = 'supfig03_a_new_infections_timeseries_ploz.png'
+#figure_filename = 'supfig03_b_new_infections_timeseries_pluk.png'
+
+
+# List of files to plot
+list_of_files = ['qld_distributed_2021-02-01_2021-03-31_iqf_0.3000_poisson_0.1000.obj',
+                 'qld_distributed_2021-02-01_2021-03-31_iqf_0.3000_poisson_0.1500.obj',
+                 'qld_distributed_2021-02-01_2021-03-31_iqf_0.3000_poisson_0.2000.obj',
+                 'qld_distributed_2021-02-01_2021-03-31_iqf_0.3000_poisson_0.2500.obj',
+                 'qld_distributed_2021-02-01_2021-03-31_iqf_0.3000_poisson_0.3000.obj',
+                 'qld_distributed_2021-02-01_2021-03-31_iqf_0.3000_poisson_0.3500.obj',
+                 'qld_distributed_2021-02-01_2021-03-31_iqf_0.3000_poisson_0.4000.obj',
+                 'qld_distributed_2021-02-01_2021-03-31_iqf_0.3000_poisson_0.4500.obj',
+                 'qld_distributed_2021-02-01_2021-03-31_iqf_0.3000_poisson_0.5000.obj',
+                 'qld_distributed_2021-02-01_2021-03-31_iqf_0.3000_poisson_0.5500.obj',
+                 'qld_distributed_2021-02-01_2021-03-31_iqf_0.3000_poisson_0.6000.obj']
+
+                  
+def format_ax(ax, sim, start_day_idx=0, key=None):
+    @ticker.FuncFormatter
+    def date_formatter(x, pos):
+        return (sim['start_day'] + dt.timedelta(days=x)).strftime('%b-%d')
+    ax.xaxis.set_major_formatter(date_formatter)
+    if key != 'r_eff':
+        sc.commaticks()
+    pl.xlim([start_day_idx, sim['n_days']-7])
+    #sc.boxoff()
+    return
+
+def plotter(key, sims, ax, label='', ylabel='', low_q=0.025, high_q=0.975, main_colour=[0.0, 0.0, 0.0], start_day=None, subsample=2, choose_run=0):
+    
+    ys = []
+    for this_sim in sims:
+        ys.append(this_sim.results[key].values)
+    yarr = np.array(ys)
+
+    if choose_run is not None:
+        single_sim = sims[choose_run].results[key].values
+
+    low  = pl.quantile(yarr, q=low_q, axis=0)
+    high = pl.quantile(yarr, q=high_q, axis=0)
+    halfsies = pl.quantile(yarr, q=0.5, axis=0)
+
+    single_sim = sims[0] # For having a sim to refer to regarding time
+    tvec = np.arange(len(low))
+    
+    start_day_idx = single_sim.day(start_day)
+    start_day_fill = single_sim.day('2021-02-02')
+    end_day_idx = -1
+    
+
+    pl.plot(tvec[start_day_fill:end_day_idx], yarr[0:-1:50, start_day_fill:end_day_idx].T, c=[0.5, 0.5, 0.5], alpha=0.05)
+    #pl.plot(tvec[start_day_idx:start_day_fill+1], yarr[:, start_day_idx:start_day_fill+1].T, c=[0.0, 0.0, 0.0], alpha=0.05)
+
+    #pl.plot(tvec[start_day_idx:start_day_fill+1], halfsies[start_day_idx:start_day_fill+1], c=[0.0, 0.0, 0.0], alpha=0.7)
+    pl.plot(tvec[start_day_fill:end_day_idx], halfsies[start_day_fill:end_day_idx], c=[0.5, 0.5, 0.5], label=label, lw=4, alpha=0.5)
+    pl.plot(tvec[start_day_fill:end_day_idx], halfsies[start_day_fill:end_day_idx], c=main_colour, label=label, lw=2, alpha=1.0)
+
+    xmin, xmax = ax.get_xlim()
+    pl.ylabel(ylabel)
+
+
+# Fonts and sizes
+font_size = 16
+pl.rcParams['font.size'] = font_size
+
+@ticker.FuncFormatter
+def date_formatter(x, pos):
+    return (dt.date(2021,2,1) + dt.timedelta(days=x)).strftime('%b-%d')
+
+# Plot diagnoses
+this_fig, ax1 = plt.subplots(figsize=(9, 5))
+
+
+import matplotlib.cm as cm
+import matplotlib as mpl 
+
+num_cases = len(list_of_files)
+num_cases = num_cases
+
+cmap = cm.get_cmap('YlOrRd', num_cases+1)
+
+norm_cbar = mpl.colors.Normalize(vmin=0,vmax=num_cases)     
+
+sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm_cbar)
+sm.set_array([])
+
+start_display_day = '2021-02-02' 
+# Load the data
+pl.plot([0, 58], [5, 5], c=[0.0, 0.0, 0.0], lw=8, alpha=0.5)
+
+for file_idx, this_file in enumerate(list_of_files):
+    msim = sc.loadobj(f'{results_folder}/{this_file}')
+    sims = msim.sims
+    format_ax(ax1, sims[0], start_day_idx=sims[0].day(start_display_day))
+    main_colour =  list(cmap(file_idx/len(list_of_files)))[0:-1]
+    plotter('new_infections', sims, ax1, label='model predictions', ylabel=r'new daily infections', start_day=start_display_day, main_colour=main_colour)
+
+#plt.yscale('log')
+plt.ylim([0.0, 25])
+plt.xlim([0, 58])
+
+
+#plot_intervs(sims[0])
+cbar = plt.colorbar(sm, ticks=np.linspace(0.5, num_cases-0.5, num_cases), 
+                        boundaries=np.arange(0, num_cases+1, 1))
+
+cbar_labels = [f'{label:.2f}' for label in np.linspace(0.1, 0.6, num_cases)]
+
+cbar.ax.get_yaxis().labelpad = 40
+cbar.ax.set_ylabel('daily rate of \n imported infections', rotation=270)
+cbar.ax.set_yticklabels(cbar_labels)  # vertically oriented colorbar
+ax = plt.gca();
+ax.annotate('A', xy=(0.02, 0.9125), xycoords='figure fraction', fontsize=32)
+ax.annotate('A.2.2', xy=(1., 23), xycoords='data', fontsize=18)
+
+#ax.annotate('B', xy=(0.02, 0.9125), xycoords='figure fraction', fontsize=32)
+#ax.annotate('B.1.1.7', xy=(1., 23), xycoords='data', fontsize=18)
+
+ax.annotate('SCT threshold',
+            xy=(2., 5.5), xycoords='data',
+            xytext=(10, 30), textcoords='offset points',
+            arrowprops=dict(arrowstyle="->", connectionstyle="angle,angleA=-180,angleB=90,rad=10"),fontsize=18)
+plt.tight_layout()
+import covasim as cv
+cv.savefig("/".join((figure_folder, figure_filename)), dpi=300)
+plt.show()
---
+++ qld-model/plotting/paper-figures/supfig05_ab_prob_sct_detection_a22_vs_b117.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import covasim as cv
+
+sns.set_context("poster", font_scale=0.9)
+
+# Import data - x4 main scenarios 
+df_cloz = pd.read_csv('/home/paula/Dropbox/COVID/simulated-data/resurgence/outbreak_cluster_size_oz.csv')
+df_cluk = pd.read_csv('/home/paula/Dropbox/COVID/simulated-data/resurgence/outbreak_cluster_size_uk.csv')
+df_ploz = pd.read_csv('/home/paula/Dropbox/COVID/simulated-data/resurgence/outbreak_poisson_lambda_oz.csv')
+df_pluk = pd.read_csv('/home/paula/Dropbox/COVID/simulated-data/resurgence/outbreak_poisson_lambda_uk.csv')
+
+# Select one lvel of testing and one level of iq
+num_tests = 6260
+iq_factor = 0.5
+cluster_size_th = 15
+cluster_col = "cluster_size"
+poisson_th = 1.3
+poisson_col = "poisson_lambda"
+which_outbreak_prob="outbreak_prob"
+which_colourmap = "inferno"
+
+def get_subframe(df, num_tests, column_name, col_th):
+    df_sub = df[df["num_tests"] == num_tests]
+    if column_name == "poisson_lambda":
+        df_sub = df_sub[(df_sub[column_name] <=col_th) & (df_sub[column_name] >=0.5)]
+    else:
+        df_sub = df_sub[df_sub[column_name] <=col_th]
+
+    df_sub = df_sub[["iq_factor", which_outbreak_prob, column_name]]
+    df_map = df_sub.pivot("iq_factor", column_name, which_outbreak_prob)
+    return df_map
+
+def plot_heatmaps(df_map_list, fig_name_list, fig_labels=["A", "B"], xlabel='cluster size'):
+    idx=0
+    for df_map, fig_name in zip(df_map_list, fig_name_list):
+        f, ax = plt.subplots(figsize=(14, 9))
+        sns.heatmap(df_map, annot=True, fmt=".0f", linewidths=.5, ax=ax, cmap=which_colourmap, vmin=0, vmax=100, cbar_kws={'label': 'probabilty [%]'})
+        ax.set_ylabel('Q/I leakage')
+        ax.set_xlabel(xlabel)
+        ax.annotate(fig_labels[idx], xy=(0.02, 0.9125), xycoords='figure fraction', fontsize=50)
+        idx+=1
+        f.tight_layout()
+        cv.savefig(fig_name, dpi=300)
+    return
+
+
+figure_folder = '/home/paula/Work/Articles/coronavirus-qld-calibration/figures'
+
+dfcloz_map = get_subframe(df_cloz, num_tests, cluster_col, cluster_size_th) 
+dfcluk_map = get_subframe(df_cluk, num_tests, cluster_col, cluster_size_th) 
+
+plot_heatmaps([dfcloz_map, dfcluk_map], [f"{figure_folder}/fig_prob_outbreak_de_map_cluster_oz.png", f"{figure_folder}/fig_prob_outbreak_de_map_cluster_uk.png"])
+
+dfploz_map = get_subframe(df_ploz, num_tests, poisson_col, poisson_th) 
+dfpluk_map = get_subframe(df_pluk, num_tests, poisson_col, poisson_th) 
+
+plot_heatmaps([dfploz_map, dfpluk_map], [f"{figure_folder}/fig_prob_outbreak_de_map_poisson_oz.png", f"{figure_folder}/fig_prob_outbreak_de_map_poisson_uk.png"], fig_labels=["C", "D"], xlabel='daily rate\nimported infections')
+
+plt.show()
