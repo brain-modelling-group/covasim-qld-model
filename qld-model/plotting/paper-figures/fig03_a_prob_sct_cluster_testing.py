9b74d9276df917b774441a4b08c3d5e9ba3c04c9
Paula Sanz-Leon <paula.sanz-leon@qimrberghofer.edu.au>
2021-06-23 11:45:47 +1000

    Update style

--- qld-model/plotting/paper-figures/fig03_a_prob_sct_cluster_testing.py
+++ qld-model/plotting/paper-figures/fig03_a_prob_sct_cluster_testing.py
@@ -12,8 +12,8 @@ sns.set_context("paper", font_scale=1.5)
 
 # Import data 
 #df_cluk = pd.read_csv('/home/paula/data_ext4/Dropbox/COVID/simulated-data/resurgence/outbreak_cluster_size_uk.csv')
-df_cluk = pd.read_csv('/home/paula/data_ext4/Dropbox/COVID/simulated-data/resurgence/outbreak_cluster_size_uk.csv')
-df_cloz = pd.read_csv('/home/paula/data_ext4/Dropbox/COVID/simulated-data/resurgence/outbreak_cluster_size_oz.csv')
+df_cluk = pd.read_csv('/home/paula/Dropbox/COVID/simulated-data/resurgence/outbreak_cluster_size_uk.csv')
+df_cloz = pd.read_csv('/home/paula/Dropbox/COVID/simulated-data/resurgence/outbreak_cluster_size_oz.csv')
 
 def get_subframe(df, num_tests, iq_factor):
     return df[(df["num_tests"] == num_tests) & (df["iq_factor"] == iq_factor)]
@@ -40,7 +40,7 @@ for idx, nt in enumerate([6260]):
 #ax1.plot(data["cluster_size"], [50]*data["cluster_size"].shape[0], color='#fed976', lw=6, alpha=1.0)
 #ax1.plot(data["cluster_size"], [70]*data["cluster_size"].shape[0], color='#fc4e2a', lw=6, alpha=1.0)
 #ax1.plot(data["cluster_size"], [90]*data["cluster_size"].shape[0], color='#b10026', lw=6, alpha=1.0)
-ls1.append(ax1.plot(data["cluster_size"], data["resurgence_prob"], color='black', lw=0.5,  marker='o', ms=10, label="~6,000 - A.2.2"))
+ls1.append(ax1.plot(data["cluster_size"], data["resurgence_prob"], color=[0.5,0.5,0.5], ls='-', lw=2, label="~6,000 - A.2.2"))
 
 # ax1.annotate(
 #     '90%',
@@ -60,22 +60,21 @@ ls1.append(ax1.plot(data["cluster_size"], data["resurgence_prob"], color='black'
 #     xytext=(-40, 0), textcoords='offset points',
 #     bbox=dict(boxstyle="round", fc="#fed976", alpha=1.0))
 
-category_colors = plt.get_cmap('Greys')(np.linspace(0.0, 1.0, 9))
+category_colors = plt.get_cmap('Reds_r')(np.linspace(0.0, 1.0, 9))
 for idx, nt in enumerate([6260, 8360, 12560, 31460, 107060]):
     data = get_subframe(df_cluk, nt, iq_factor)
     ls1.append(ax1.plot(data["cluster_size"], data["resurgence_prob"], color=category_colors[idx+2, ...]*0.8, lw=2, label="~"+fake_labels[idx]+" - B.1.1.7"))
 
 
 
-#ax1.annotate("A", xy=(0.02, 0.9125), xycoords='figure fraction', fontsize=22)
+ax1.annotate("A", xy=(0.02, 0.9125), xycoords='figure fraction', fontsize=22)
 
 # Labels for legend
 handler1, label1 = ax1.get_legend_handles_labels()
 ax1.legend(handler1, label1, loc="lower right", frameon=True, title='number of daily tests')
 fig.tight_layout()
 #figure_folder = '/home/paula/Work/Articles/coronavirus-qld-calibration/figures'
-figure_folder = '/home/paula/data_ext4/Dropbox/COVID/articles/coronavirus-qld-calibration/figures'
-
+figure_folder = '/home/paula/Dropbox/COVID/articles/coronavirus-qld-calibration/figures'
 cv.savefig(f"{figure_folder}/fig03_a_prob_sct_cluster_tests_iq_0.1_uk-oz.png", dpi=300)
 
 plt.show()
--- qld-model/plotting/paper-figures/fig03_b_prob_sct_cluster_vax.py
+++ qld-model/plotting/paper-figures/fig03_b_prob_sct_cluster_vax.py
@@ -15,7 +15,6 @@ df_cloz = pd.read_csv('/home/paula/Dropbox/COVID/simulated-data/resurgence/outbr
 def get_oz_subframe(df, num_tests, iq_factor):
     return df[(df["num_tests"] == num_tests) & (df["iq_factor"] == iq_factor)]
 
-
 def get_subframe(df, vax_prop, iq_factor):
     return df[(df["vax_proportion"] == vax_prop) & (df["iq_factor"] == iq_factor)]
 
@@ -24,7 +23,7 @@ def get_subsubframe(df, vax_eff):
 
 # Select one lvel of testing and one level of iq
 iq_factor = 0.1
-vax_prop  = 0.5
+vax_eff  = 0.7
 
 fig, ax1 = plt.subplots(figsize=(9,5.5))
 ax1.set_xlabel('cluster size')
@@ -34,59 +33,41 @@ ax1.set_ylim([0, 100])
     
 
 ls1 = []
-data_oz = get_oz_subframe(df_cloz, 6260, iq_factor)
-data = get_subframe(df_ukvax, vax_prop, iq_factor)
+ls2 = []
 
-# Get baseline case
-data_baseline =  get_subsubframe(get_subframe(df_ukvax, 0.0, 0.1), 0.0)   
+# Get anscestral case
+data_oz = get_oz_subframe(df_cloz, 6260, iq_factor)
+# Get baseline uk case
+data_baseline =  get_subsubframe(get_subframe(df_ukvax, 0.0, iq_factor), 0.0)   
+
+category_colors = plt.get_cmap('coolwarm_r')(np.linspace(0.0, 1.0, 7))
 
 # Reference cases - no vax - A22 and B117
-ls1.append(ax1.plot(data_oz["cluster_size"], data_oz["resurgence_prob"], color='black', lw=0.5,  marker='o', ms=10, label="vp, ve = (0%, 0%) - A.2.2"))
-ls1.append(ax1.plot(data_baseline["cluster_size"], data_baseline["sct_prob"], color='red', lw=0.5,  marker='o', ms=10, label="vp, ve = (0%, 0%) - B.1.1.7"))
+ls1.append(ax1.plot(data_oz["cluster_size"], data_oz["resurgence_prob"], color=category_colors[-1, ...], lw=3,  label="A.2.2"))
+ls1.append(ax1.plot(data_baseline["cluster_size"], data_baseline["sct_prob"], color=category_colors[0, ...], lw=3,label="B.1.1.7"))
 
-category_colors = plt.get_cmap('coolwarm_r')(np.linspace(0.0, 1.0, 121))
-color_idx = [14, 16, 18, 20, 58, 60, 62, 64, 84, 86, 119, 120]
+category_colors = plt.get_cmap('coolwarm_r')(np.linspace(0.0, 1.0, 7))
 
-fake_labels = ['vp, ve = (10%, 30%)', 'vp, ve = (10%, 50%)', 'vp, ve = (10%, 70%)', 'vp, ve = (10%, 90%)',
-               'vp, ve = (50%, 30%)', 'vp, ve = (50%, 50%)', 'vp, ve = (50%, 70%)', 'vp, ve = (50%, 90%)', 
-               'vp, ve = (70%, 70%)', 'vp, ve = (70%, 90%)',
-               'vp, ve = (100%, 90%)', 'vp, ve = (100%, 100%)']
-c_idx = 0  
-
-for idx, ve in enumerate([0.3, 0.5, 0.7, 0.9]):
-    data = get_subsubframe(get_subframe(df_ukvax, 0.1, iq_factor), ve) 
-    ls1.append(ax1.plot(data["cluster_size"], data["sct_prob"], color=category_colors[color_idx[c_idx], ...]*0.8, lw=2, label=fake_labels[c_idx]))
-    c_idx +=1
-
-for idx, ve in enumerate([0.3, 0.5, 0.7, 0.9]):
-    data = get_subsubframe(get_subframe(df_ukvax, 0.5, iq_factor), ve) 
-    ls1.append(ax1.plot(data["cluster_size"], data["sct_prob"], color=category_colors[color_idx[c_idx], ...]*0.8, lw=2, label=fake_labels[c_idx]))
-    c_idx +=1
-
-for idx, ve in enumerate([0.7, 0.9]):
-    data = get_subsubframe(get_subframe(df_ukvax, 0.7, iq_factor), ve) 
-    ls1.append(ax1.plot(data["cluster_size"], data["sct_prob"], color=category_colors[color_idx[c_idx], ...]*0.8, lw=2, label=fake_labels[c_idx]))
-    c_idx +=1
-
-for idx, ve in enumerate([0.9]):
-    data = get_subsubframe(get_subframe(df_ukvax, 1.0, iq_factor), ve) 
-    ls1.append(ax1.plot(data["cluster_size"], data["sct_prob"], color=category_colors[color_idx[c_idx], ...]*0.8, lw=2, label=fake_labels[c_idx]))
-    c_idx +=1
-
-for idx, ve in enumerate([1.0]):
-    data = get_subsubframe(get_subframe(df_ukvax, 1.0, iq_factor), ve) 
-    ls1.append(ax1.plot(data["cluster_size"], data["sct_prob"], color=category_colors[color_idx[c_idx], ...]*0.8, lw=2, label=fake_labels[c_idx]))
+fake_labels = ['vc = 20%', 'vc = 50%', 'vc = 70%']
+color_idx = [1, 4, 5] 
+c_idx = 0
+for idx, vp in enumerate([0.2, 0.5, 0.7]):
+    data = get_subsubframe(get_subframe(df_ukvax,vp, iq_factor), vax_eff) 
+    ls2.append(ax1.plot(data["cluster_size"], data["sct_prob"], color=category_colors[color_idx[c_idx], ...], lw=2, label=fake_labels[c_idx]))
     c_idx +=1
 
 ax1.annotate("B", xy=(0.02, 0.9125), xycoords='figure fraction', fontsize=22)
 
 # # Labels for legend
 handler1, label1 = ax1.get_legend_handles_labels()
-ax1.legend(handler1, label1, loc="lower right", frameon=True, title='')
+leg2 = ax1.legend(handler1[2:], label1[2:], loc="lower right", frameon=False, title='vaccination efficacy 70%', fontsize=14)
+leg1 = ax1.legend(handler1[0:2], label1[0:2], loc=(0.68, 0.325), frameon=False, title='no vaccination ', fontsize=14)
+ax1.add_artist(leg2)
 fig.tight_layout()
 #figure_folder = '/home/paula/Work/Articles/coronavirus-qld-calibration/figures'
 figure_folder = '/home/paula/Dropbox/COVID/articles/coronavirus-qld-calibration/figures'
 
-#cv.savefig(f"{figure_folder}/fig03_b_prob_sct_cluster_vax_iq_0.1_uk.png", dpi=300)
+import pdb; pdb.set_trace()
+cv.savefig(f"{figure_folder}/fig03_b_prob_sct_cluster_vax_iq_0.1_uk.png", dpi=300)
 
 plt.show()
